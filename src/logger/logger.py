eb70c79be791ae78186cc4f150a6b64e83a900e0
Lucas Reisener <lucasreisener@gmail.com>
2023-03-15 15:42:43 +0100

    added return type annotations to interfaces

--- src/logger/logger.py
+++ src/logger/logger.py
@@ -1,6 +1,7 @@
 """
 This module contains the logger class
 """
+from typing import Type
 
 
 class Logger:
@@ -13,7 +14,7 @@ class Logger:
     def __init__(self, connection_string):
         self.connection_string = connection_string
 
-    def spawn_train(self, train_id: int):
+    def spawn_train(self, train_id: int) -> Type[None]:
         """
         This function should be called when a train is beeing spawned. This should include a train
         identifier, the type of train, the route (if not specified by schedule), and the position.
@@ -21,7 +22,7 @@ class Logger:
         :rtype: None
         """
 
-    def remove_train(self, train_id: int):
+    def remove_train(self, train_id: int) -> Type[None]:
         """
         This function should be called when a train is beeing removed. This should include a train
         identifier, the type of train, and the position.
@@ -29,7 +30,7 @@ class Logger:
         :rtype: None
         """
 
-    def arrival_train(self, train_id: int, station_id: int):
+    def arrival_train(self, train_id: int, station_id: int) -> Type[None]:
         """
         This function should be called when a train arrives at a station. This should include a
         train identifier, the type of train, and the station identifier, maybe platform (?).
@@ -38,7 +39,7 @@ class Logger:
         :rtype: None
         """
 
-    def departure_train(self, train_id: int, station_id: int):
+    def departure_train(self, train_id: int, station_id: int) -> Type[None]:
         """
         This function should be called when a train departes from a station. This should include a
         train identifier, the type of train, and the station identifier, maybe platform (?).
@@ -47,7 +48,7 @@ class Logger:
                 :rtype: None
         """
 
-    def create_fahrstrasse(self, fahrstrasse: any):
+    def create_fahrstrasse(self, fahrstrasse: any) -> Type[None]:
         """
         This function should be called when a Fahrstrasse is beeing formed. This should include the
         definition of the Fahrstrasse.
@@ -55,7 +56,7 @@ class Logger:
         :rtype: None
         """
 
-    def remove_fahrstrasse(self, fahrstrasse: any):
+    def remove_fahrstrasse(self, fahrstrasse: any) -> Type[None]:
         """
         This function should be called when a Fahrstrasse is beeing dissolved. This should include
         the definition of the Fahrstrasse.
@@ -63,7 +64,9 @@ class Logger:
         :rtype: None
         """
 
-    def set_signal(self, signal_id: int, state_before: int, state_after: int):
+    def set_signal(
+        self, signal_id: int, state_before: int, state_after: int
+    ) -> Type[None]:
         """
         This function is beeing called when setting a signal or changing its state. This should
         include the signal identifier, the state before and the state after the change.
@@ -75,7 +78,7 @@ class Logger:
 
     def inject_fault(
         self, injection_id: int, fault_type: int, injection_position: any, duration: int
-    ):
+    ) -> Type[None]:
         """
         This function should be called when injecting a fault into the simulation. This should
         include (the injection id), type of injection, position of injection, duration and some
@@ -87,7 +90,7 @@ class Logger:
         :rtype: None
         """
 
-    def remove_fault(self, injection_id: int):
+    def remove_fault(self, injection_id: int) -> Type[None]:
         """
         This function should be called when removing a fault from the simulation. This should
         reference the injection id, that was removed.
