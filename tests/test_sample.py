2de4c60e50dd28bcd2351244d86cc2a73c391b57
Lucas Reisener <lucasreisener@gmail.com>
2023-03-16 16:28:45 +0100

    fix test sample lint

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,6 +1,20 @@
-def inc(x):
-    return x + 1
+"""
+A basic test sample
+"""
+
+def inc(num : int):
+    """
+    A basic increment function
+
+    :rtype: int
+    """
+    return num + 1
 
 
 def test_answer():
+    """
+    A basic test
+
+    :return: None
+    """
     assert inc(4) == 5
