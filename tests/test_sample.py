a5bc01257d5c7d2f555e87d47b7f5ec7b66f7f2c
Lucas Reisener <lucasreisener@gmail.com>
2023-03-16 16:17:35 +0100

    Revert "update test sample to fix lint"
    This reverts commit fbecf16a65f99e29005a6915711407e4719ae18d.

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,20 +1,6 @@
-"""
-A basic test sample
-"""
-
-def inc(num : int):
-    """
-    A basic increment function
-
-    :rtype: int
-    """
-    return num + 1
+def inc(x):
+    return x + 1
 
 
 def test_answer():
-    """
-    A basic test
-
-    :return: None
-    """
     assert inc(4) == 5
