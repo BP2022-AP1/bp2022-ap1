22e5ba63219c1f60fb478d93cd6b369dcdacbf97
Christian R <62649536+CR1337@users.noreply.github.com>
2023-03-20 14:33:12 +0100

    fixed typo

--- src/schedule/schedule_strategy.py
+++ src/schedule/schedule_strategy.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 class ScheduleStrategy(ABC):
     """An abstract ScheduleStrategy.
     ScheduleStartegies implement the decision logic of
-    how a schedule determines whether a vehicle shoudl be spawned
+    how a schedule determines whether a vehicle should be spawned
     at a given tick.
     """
 
