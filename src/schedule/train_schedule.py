d20d5d0f5f495dff14e39618ea892349ed7dd3ef
Christian R <62649536+CR1337@users.noreply.github.com>
2023-03-20 14:37:46 +0100

    Feature/30 spawner schedule skeleton (#50)
    * added some init files
    
    * ignore vscode settings
    
    * added class docstring
    
    * implemented skeleton
    
    * formatted with black
    
    * fixed typo

---
+++ src/schedule/train_schedule.py
@@ -0,0 +1,25 @@
+from src.schedule.schedule import Schedule
+
+
+class TrainSchedule(Schedule):
+    """A schedule for spawning SUMO trains."""
+
+    @classmethod
+    def from_json(cls, json_object: str) -> "TrainSchedule":
+        """Constructs a TrainSchedule from a JSON object.
+
+        :param json_object: The JSON object.
+        :type json_object: str
+        :return: A TrainSchedule.
+        :rtype: TrainSchedule
+        """
+        raise NotImplementedError()  # TODO: implement this
+
+    def maybe_spawn(self, tick: int):
+        """Determines whether a train should be spawned at the
+        current tick and spawns it if so.
+
+        :param tick: The current tick.
+        :type tick: int
+        """
+        raise NotImplementedError()  # TODO: implement this
