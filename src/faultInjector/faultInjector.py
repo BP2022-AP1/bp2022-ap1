3418d505ae33536ab9d265b80358933f30a79028
Anton Persitzky <82519211+instant-sky@users.noreply.github.com>
2023-03-21 10:09:12 +0100

    add faultInjector class and add first two methods

---
+++ src/faultInjector/faultInjector.py
@@ -0,0 +1,32 @@
+"""
+This module contains the fault injector class
+"""
+
+
+from typing import Type
+
+from src.component import Component
+
+
+class FaultInjector(Component):
+    """
+    Class for fault injection. Faults can be injected into the interlockingController, the Spawner and the Wrapper
+    """
+
+    def add_fault_from_json(self, json_faults: list[str]):
+        """Adds faults that should be injected to the fault injector
+
+        :param json_faults: The list of faults as JSON objects
+        :type json_faults: list[str]
+        """
+
+        raise NotImplementedError()
+    
+    def next_tick(self, tick: int):
+        """Called by the wrapper to announce the next tick of the simulation
+
+        :param tick: The current simulation tick
+        :type tick: int
+        """
+
+        raise NotImplementedError()
