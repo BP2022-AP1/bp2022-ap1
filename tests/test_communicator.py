3a4cf1bcd245b1892ee6418714cfcd5c2b2f94bb
Luke <37272151+SaturnHafen@users.noreply.github.com>
2023-03-22 15:27:59 +0100

    feat: Add test to ckeck if next_tick is called

--- tests/test_communicator.py
+++ tests/test_communicator.py
@@ -1,6 +1,19 @@
 from time import sleep
 
+from src.component import Component
 from src.wrapper.communicator import Communicator
+
+
+class MockComponent(Component):
+    """Mock for a simple component to check if next tick is called"""
+
+    call_count = 0
+
+    def __init__(self):
+        Component.__init__(self, None, 1)
+
+    def next_tick(self, tick: int):
+        self.call_count += 1
 
 
 def test_simulation_runs():
@@ -9,3 +22,22 @@ def test_simulation_runs():
     sleep(0.1)
     communicator.stop()
     assert communicator.progress > 0
+
+
+def test_component_next_tick_is_called():
+    mock = MockComponent()
+    communicator = Communicator(components=[mock])
+    communicator.start()
+    sleep(1.1)
+    communicator.stop()
+    assert mock.call_count > 0
+
+
+def test_component_next_tick_is_called_late_add():
+    mock = MockComponent()
+    communicator = Communicator()
+    communicator.start()
+    communicator.add_component(mock)
+    sleep(1.1)
+    communicator.stop()
+    assert mock.call_count > 0
