1d9e5e63ee5b82032f0d4e6308118fff35f50dba
Anton Persitzky <82519211+instant-sky@users.noreply.github.com>
2023-03-23 15:33:15 +0100

    fix: lint

--- tests/test_fault_injector.py
+++ tests/test_fault_injector.py
@@ -20,16 +20,14 @@ def test_json_schema_1():
 
 def test_json_schema_2():
     test_fault = TrainFault()
-    assert (
-        test_fault.check_json(
-            json.loads(
-                """{
+    assert test_fault.check_json(
+        json.loads(
+            """{
                     "faultID" : 2, 
                     "startTick" : 2, 
                     "endTick" : 3,
                     "affectedElementID" : 4, 
                     "description" : "2"
                 }"""
-            )
         )
     )
