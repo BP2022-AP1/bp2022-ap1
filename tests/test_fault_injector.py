b439b017b62e15b7a40e8222b5097602c478dbe0
Anton Persitzky <82519211+instant-sky@users.noreply.github.com>
2023-03-23 15:09:28 +0100

    style: improve styling of the json tests

--- tests/test_fault_injector.py
+++ tests/test_fault_injector.py
@@ -3,25 +3,33 @@ import json
 from src.fault_injector.fault_types.train_fault import TrainFault
 
 
-def test_json_schema():
+def test_json_schema_1():
     test_fault = TrainFault()
-    assert (
-        test_fault.check_json(
-            json.loads(
-                '{"faultID" : 1, "startTick" : 2, "endTick" : 3, "affectedElementID" : 4, "description" : "2"}'
-            )
+    assert test_fault.check_json(
+        json.loads(
+            """{
+                    "faultID" : 1, 
+                    "startTick" : 2, 
+                    "endTick" : 3, 
+                    "affectedElementID" : 4, 
+                    "description" : "2"
+                }"""
         )
-        == False
     )
 
 
-def test_json_schema():
+def test_json_schema_2():
     test_fault = TrainFault()
-    assert (
+    assert not (
         test_fault.check_json(
             json.loads(
-                '{"faultID" : 2, "startTick" : 2, "endTick" : 3, "affectedElementID" : 4, "description" : "2"}'
+                """{
+                    "faultID" : 2, 
+                    "startTick" : 2, 
+                    "endTick" : 3,
+                    "affectedElementID" : 4, 
+                    "description" : "2"
+                }"""
             )
         )
-        == True
     )
