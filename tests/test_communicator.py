b8aeb11f184b52b07905b45dba73e54a37cdb7bb
Luke <37272151+SaturnHafen@users.noreply.github.com>
2023-03-23 09:26:29 +0100

    Feat: migrate tests to mock-object

--- tests/test_communicator.py
+++ tests/test_communicator.py
@@ -1,7 +1,32 @@
 from time import sleep
 
+import pytest
+import traci
+
 from src.component import Component
 from src.wrapper.communicator import Communicator
+
+
+@pytest.fixture
+def mock_traci(monkeypatch):
+    def get_all_subscription_results():
+        return {}
+
+    def simulation_step(*args, **kwargs):  # pylint: disable=unused-argument
+        return
+
+    def start(*args, **kwargs):  # pylint: disable=unused-argument
+        return
+
+    def close(*args, **kwargs):  # pylint: disable=unused-argument
+        return
+
+    monkeypatch.setattr(
+        traci.simulation, "getAllSubscriptionResults", get_all_subscription_results
+    )
+    monkeypatch.setattr(traci, "simulationStep", simulation_step)
+    monkeypatch.setattr(traci, "start", start)
+    monkeypatch.setattr(traci, "close", close)
 
 
 class MockComponent(Component):
@@ -16,7 +41,9 @@ class MockComponent(Component):
         self.call_count += 1
 
 
-def test_simulation_runs():
+def test_simulation_runs(
+    mock_traci,
+):  # pylint: disable=redefined-outer-name,unused-argument
     communicator = Communicator()
     communicator.start()
     while not communicator.sumo_running:
@@ -25,7 +52,9 @@ def test_simulation_runs():
     assert communicator.progress > 0
 
 
-def test_component_next_tick_is_called():
+def test_component_next_tick_is_called(
+    mock_traci,
+):  # pylint: disable=redefined-outer-name,unused-argument
     mock = MockComponent()
     communicator = Communicator(components=[mock])
     communicator.start()
@@ -35,7 +64,9 @@ def test_component_next_tick_is_called():
     assert mock.call_count > 0
 
 
-def test_component_next_tick_is_called_late_add():
+def test_component_next_tick_is_called_late_add(
+    mock_traci,
+):  # pylint: disable=redefined-outer-name,unused-argument
     mock = MockComponent()
     communicator = Communicator()
     communicator.start()
