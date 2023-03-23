0477cde6d72d501777b8ac6d576e846738df7111
Antony Kamp <info@antonykamp.de>
2023-03-23 16:15:09 +0100

    Feature: Setup dotenv (#70)
    * dep: install python-dotenv
    
    Install dev dependency dotenv to load env variables from files.
    
    * feat: setup dotenv
    
    Add constants file to test and src folders. These contain environment variables in the future.
    Add .env.secret.sample file to define needed env variables.
    
    * doc: replace explanation with link to wiki
    
    * feat: add type declaration
    
    Add type declaration to constants dictionary in constants.py files.
    
    * feat: add tests
    
    Add tests to verify the loading order of the environment files for the development and test environment.

---
+++ src/constants.py
@@ -0,0 +1,9 @@
+import os
+
+from dotenv import dotenv_values
+
+constants: dict[str, str | None] = {
+    **dotenv_values(".env.shared"),  # load shared development variables
+    **dotenv_values(".env.secret"),  # load sensitive variables
+    **os.environ,  # override loaded values with environment variables
+}
