24d917cbe42626f2e2b292efeac4eaa625fbe1ce
antonykamp <info@antonykamp.de>
2023-03-23 14:50:01 +0100

    feat: add type declaration
    Add type declaration to constants dictionary in constants.py files.

--- src/constants.py
+++ src/constants.py
@@ -2,7 +2,7 @@ import os
 
 from dotenv import dotenv_values
 
-constants = {
+constants: dict[str, str | None] = {
     **dotenv_values(".env.shared"),  # load shared development variables
     **dotenv_values(".env.secret"),  # load sensitive variables
     **os.environ,  # override loaded values with environment variables
