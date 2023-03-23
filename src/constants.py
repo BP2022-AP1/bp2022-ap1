0d658a36c939e76999808002e10d28e965c8a86c
antonykamp <info@antonykamp.de>
2023-03-23 19:07:03 +0100

    feat: replace constants.py with loading variables
    Remove importing dotenv values into constants.py.
    Add loading dotenv values to configuration file for future flask application.

--- src/constants.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from dotenv import dotenv_values
-
-constants: dict[str, str | None] = {
-    **dotenv_values(".env.shared"),  # load shared development variables
-    **dotenv_values(".env.secret"),  # load sensitive variables
-    **os.environ,  # override loaded values with environment variables
-}
