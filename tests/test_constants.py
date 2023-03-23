fca339b5ec6a63d2056b372ff123a7bd123bacd7
antonykamp <info@antonykamp.de>
2023-03-23 19:03:00 +0100

    test: don't test test-envs, mock loading env variables
    Mock and test load_dotenv.
    Don't test for loaded test env variables.

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -10,55 +10,34 @@ def mock_dotenv(monkeypatch):
     Mock dotenv to return self defined environment file content.
     """
 
-    def dotenv_values_mock(env_file: str) -> dict[str, str | None]:
+    mocked_shared_env = {"v1": "shared", "v2": "shared"}
+    mocked_secret_env = {"v1": "secret"}
+
+    def add_env_from_dict(env_dict: dict[str, str | None]) -> None:
+        for key, value in env_dict.items():
+            os.environ[key] = value
+
+    def load_dotenv_mock(env_file: str) -> dict[str, str | None]:
         match env_file:
             case ".env.shared":
-                return {"v1": "shared", "v2": "shared", "v3": "shared"}
+                add_env_from_dict(mocked_shared_env)
             case ".env.secret":
-                return {"v1": "secret", "v2": "secret"}
-            case ".env.test":
-                return {"v1": "test", "v2": "test"}
+                add_env_from_dict(mocked_secret_env)
 
-    monkeypatch.setattr(dotenv, "dotenv_values", dotenv_values_mock)
-
-
-@pytest.fixture
-def mock_os(monkeypatch):
-    """
-    Mock environment variables imported from os to return self defined environment variables.
-    """
-
-    mock_environ = {"v1": "os"}
-    monkeypatch.setattr(os, "environ", mock_environ)
+    monkeypatch.setattr(dotenv, "load_dotenv", load_dotenv_mock)
 
 
 def test_env_variables_import_order(
-    mock_dotenv, mock_os
+    mock_dotenv,
 ):  # pylint: disable=redefined-outer-name,unused-argument
     """
     Test if the loading order of the environment variables are correct.
-    .env.shared can be overwritten by .env.secret and os.
-    .env.secret can be overwritten by os.
-    os cannot be overwritten.
+    os can be overwritten by .env.secret and .env.shared.
+    .env.shared can be overwritten by .env.secret.
+    .env.secret cannot be overwritten.
     """
-    # pylint: disable-next=import-outside-toplevel
-    from src.constants import constants
+    # pylint: disable-next=import-outside-toplevel,unused-import
+    import src.config
 
-    assert constants["v1"] == "os"
-    assert constants["v2"] == "secret"
-    assert constants["v3"] == "shared"
-
-
-def test_test_env_variable_import_order(
-    mock_dotenv, mock_os
-):  # pylint: disable=redefined-outer-name,unused-argument
-    """
-    Test if the loading order of the test environment variables are correct.
-    .env.test can be overwritten by os.
-    os cannot be overwritten.
-    """
-    # pylint: disable-next=import-outside-toplevel
-    from constants import constants
-
-    assert constants["v1"] == "os"
-    assert constants["v2"] == "test"
+    assert os.getenv("v1") == "secret"
+    assert os.getenv("v2") == "shared"
