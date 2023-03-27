d8627a5724e6525e81a1c201577d49511944b412
Anton Persitzky <anton.persitzky@student.hpi.uni-potsdam.de>
2023-03-27 16:22:18 +0200

    feat: add very basic IFaultInjector class

--- src/fault_injector/fault_injector.py
+++ src/fault_injector/fault_injector.py
@@ -3,12 +3,15 @@ This module contains the fault injector class
 """
 
 from src.component import Component
+from src.fault_injector.fault_types.fault import Fault
 
 
 class FaultInjector(Component):
     """
     Class for fault injection. Faults can be injected into the interlockingController, the Spawner and the Wrapper
     """
+
+    _faults: list[Fault] = []
 
     def add_fault_from_json(self, json_faults: list[str]):
         """Adds faults that should be injected to the fault injector
@@ -26,4 +29,14 @@ class FaultInjector(Component):
         :type tick: int
         """
 
+        raise NotImplementedError()
+
+
+class IFaultInjector(object):
+    """Class that provides an interface to the Faultinjector for other components"""
+
+    def next_tick(self, tick: int):
+        raise NotImplementedError()
+
+    def create():
         raise NotImplementedError()
