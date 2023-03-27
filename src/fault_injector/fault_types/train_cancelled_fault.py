37e48b462ed7d91b29a94f051b60dc4f3256d121
Anton Persitzky <anton.persitzky@student.hpi.uni-potsdam.de>
2023-03-27 13:40:06 +0200

    feat: add TrainCancelledFault class

--- src/fault_injector/fault_types/train_cancelled_fault.py
+++ src/fault_injector/fault_types/train_cancelled_fault.py
@@ -2,26 +2,28 @@ from src.component import Component
 from src.fault_injector.fault_types.fault import Fault
 
 
-class PlatformBlockedFault(Fault):
+class TrainCancelledFault(Fault):
     """A fault that blocks a platform"""
 
+    end_tick: int = -1
+
     @classmethod
-    def from_json(cls, json_object: str) -> "PlatformBlockedFault":
-        """Constructs a PlatformBlockedFault from a JSON object
+    def from_json(cls, json_object: str) -> "TrainCancelledFault":
+        """Constructs a TrainCancelledFault from a JSON object
 
         :param json_object: The JSON object
         :type json_object: str
-        :return: a PlatformBlockedFault
-        :rtype: PlatformBlockedFault
+        :return: a TrainCancelledFault
+        :rtype: TrainCancelledFault
         """
         raise NotImplementedError()
 
     def inject_fault(component: Component):
-        """inject PlatformBlockedFault into the given component
+        """inject TrainCancelledFault into the given component
 
         :param component: The component the fault should be injected into
         :type component: Component
         """
-        # - get platform by id
-        # - mark platform as blocked
+        # - get train by id
+        # - mark train as cancelled
         raise NotImplementedError()
