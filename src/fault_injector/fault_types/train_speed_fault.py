e25dae5ceb190fefd090387f0346349900b740b1
Anton Persitzky <anton.persitzky@student.hpi.uni-potsdam.de>
2023-03-27 11:30:18 +0200

    feat: extend TrainSpeedFault class

--- src/fault_injector/fault_types/train_speed_fault.py
+++ src/fault_injector/fault_types/train_speed_fault.py
@@ -5,6 +5,9 @@ from src.fault_injector.fault_types.fault import Fault
 class TrainSpeedFault(Fault):
     """A fault affecting the speed of trains."""
 
+    new_speed: int = None
+    old_speed: int = None
+
     @classmethod
     def from_json(cls, json_object: str) -> "TrainSpeedFault":
         """Constructs a TrainFault from a JSON object
@@ -22,4 +25,13 @@ class TrainSpeedFault(Fault):
         :param component: The component the fault should be injected into
         :type component: Component
         """
+        # - get train object
+        # - save the current speed of the train in old_speed
+        # - set train speed to new_speed
+        raise NotImplementedError()
+
+    def resolve_fault(component: Component):
+        # - get train object
+        # - set the train speed to old_speed
+
         raise NotImplementedError()
