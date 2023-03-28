ed3b2a6b12bca15cd7fa484d4caaeac5c1ea89c2
CR1337 <captain_2@gmx.de>
2023-03-28 16:04:03 +0200

    implemented ISpawner

---
+++ src/interfaces.py
@@ -0,0 +1,21 @@
+from typing import Any, Protocol
+
+
+class IComponent(Protocol):
+    logger: "ILogger"
+    priority: int
+
+    def next_tick(self, tick: int, callback_object: Any):
+        ...
+
+
+class ISpawner(IComponent, Protocol):
+    def add_schedule(self, schedule: "ISchedule") -> int:
+        ...
+
+    def remove_schedule(self, schedule_id: int):
+        ...
+
+    @property
+    def schedules(self) -> list["ISchedule"]:
+        ...
