a66c99f8cb1eac6e51933a2aa03e5db8ccff317e
Anton Persitzky <82519211+instant-sky@users.noreply.github.com>
2023-03-28 14:33:44 +0200

    feat: add docstrings to protocol

--- src/interfaces.py
+++ src/interfaces.py
@@ -1,8 +1,18 @@
 from typing import Protocol
 from src.fault_injector.fault_types.fault import Fault
 
-class FaultInjector(Protocol):
+class IFaultInjector(Protocol):
 
-    def add_fault(fault: Fault) -> None: ...
+    def add_fault(self, fault: Fault) -> None:
+        """Adds faults that should be injected to the fault injector
 
-    def next_tick(tick: int) -> None: ...
+        :param json_faults: The list of faults as JSON objects
+        :type json_faults: list[str]
+        """
+
+    def next_tick(self, tick: int) -> None:
+        """Called by the wrapper to announce the next tick of the simulation
+
+        :param tick: The current simulation tick
+        :type tick: int
+        """
