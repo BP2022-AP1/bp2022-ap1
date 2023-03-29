a35121838d2f64f679ad83471892b48c9945530d
Anton Persitzky <82519211+instant-sky@users.noreply.github.com>
2023-03-29 14:23:19 +0200

    feat: remove docstrings from interface

--- src/interfaces.py
+++ src/interfaces.py
@@ -4,15 +4,7 @@ from src.fault_injector.fault_types.fault import Fault
 class IFaultInjector(Protocol):
 
     def add_fault(self, fault: Fault) -> None:
-        """Adds faults that should be injected to the fault injector
-
-        :param json_faults: The list of faults as JSON objects
-        :type json_faults: list[str]
-        """
+        ...
 
     def next_tick(self, tick: int) -> None:
-        """Called by the wrapper to announce the next tick of the simulation
-
-        :param tick: The current simulation tick
-        :type tick: int
-        """
+        ...
