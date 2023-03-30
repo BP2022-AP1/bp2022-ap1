e47602efdc1ac7b08648aca5a7b6b2f2f18c1d62
Anton Persitzky <82519211+instant-sky@users.noreply.github.com>
2023-03-30 09:54:50 +0200

    feat: use spawn restrictor interface in TrainCancelledFault

--- src/fault_injector/fault_types/train_cancelled_fault.py
+++ src/fault_injector/fault_types/train_cancelled_fault.py
@@ -1,11 +1,14 @@
 from src.component import Component
 from src.fault_injector.fault_types.fault import Fault, FaultConfig
+from src.interfaces import *
 
 
 class TrainCancelledFault(Fault):
     """A fault that blocks a platform"""
 
-    def inject_fault(self, component: Component):
+    affected_element_ID: int = None
+
+    def inject_fault(self, spawn_restrictor: ISpawnerRestrictor):
         """inject TrainCancelledFault into the given component
 
         :param component: The component the fault should be injected into
@@ -13,12 +16,12 @@ class TrainCancelledFault(Fault):
         """
         # - get train by id
         # - mark train as cancelled
-        raise NotImplementedError()
+        spawn_restrictor.block_schedule(self.affected_element_ID)
 
-    def resolve_fault(self, component: Component):
+    def resolve_fault(self, spawn_restrictor: ISpawnerRestrictor):
         """resolves the previously injected TrainCancelledFault
 
         :param component: the component with the injected fault
         :type component: Component
         """
-        raise NotImplementedError()
+        spawn_restrictor.unblock_schedule(self.affected_element_ID)
