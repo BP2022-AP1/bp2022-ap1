d4507008eb875c39d398060f9c7b16cacfec3674
MaxLietze <Max.Lietze@student.hpi.de>
2023-03-30 13:10:14 +0200

    fixed Camel Case

--- src/interlocking_component/route_controller.py
+++ src/interlocking_component/route_controller.py
@@ -1,4 +1,16 @@
-class Route_Controller(Component):
+class RouteController(Component):
+    """This class coordinates the route of a train.
+    It calls the router to find a route for a train.
+    It makes sure, that the Interlocking sets fahrstrassen along those routes.
+    """
 
     def next_tick(self, tick: int):
+        """This may be called to prosses a new simulation tick.
+
+        Args:
+            tick (int): The current tick of the simulation.
+
+        Raises:
+            NotImplementedError: This has not been Implemented yet.
+        """
         raise NotImplementedError()
