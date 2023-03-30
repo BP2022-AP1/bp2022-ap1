ce9e504b868fdd0a5eeb38d33c32257b3c90a224
MaxLietze <Max.Lietze@student.hpi.de>
2023-03-30 09:53:47 +0200

    added IInterlocking Protocol

--- src/interfaces.py
+++ src/interfaces.py
@@ -38,3 +38,8 @@ class ISpawnerRestrictor(Protocol):
 
     def unblock_schedule(self, schedule_id: int):
         ...
+
+
+class IInterlocking(Protocol):
+    def next_tick(self, tick: int):
+        ...
