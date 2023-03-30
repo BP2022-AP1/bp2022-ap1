db922745344fe70ca2a2a1cb56c56eb7b62515bc
MaxLietze <Max.Lietze@student.hpi.de>
2023-03-30 14:33:05 +0200

    Docstrings now uses sphinx format

--- src/interlocking_component/route_controller.py
+++ src/interlocking_component/route_controller.py
@@ -7,10 +7,8 @@ class RouteController(Component):
     def next_tick(self, tick: int):
         """This may be called to prosses a new simulation tick.
 
-        Args:
-            tick (int): The current tick of the simulation.
-
-        Raises:
-            NotImplementedError: This has not been Implemented yet.
+        :param tick: The current tick of the simulation.
+        :type tick: int
+        :raises NotImplementedError: This has not yet been implemented.
         """
         raise NotImplementedError()
