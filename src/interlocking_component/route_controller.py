95bafd1dc456be14d23bfba1bd0b8892aa10b2ee
MaxLietze <Max.Lietze@student.hpi.de>
2023-03-31 11:04:35 +0200

    Changed the arguments from objects to object ids

--- src/interlocking_component/route_controller.py
+++ src/interlocking_component/route_controller.py
@@ -17,56 +17,56 @@ class RouteController(Component):
 class IInterlockingDisruptor:
     """This class is the Interface to inject faults into the interlocking as well as to notify the Interlocking faults that occur in other parts of the simulation."""
 
-    def insertTrackBlocked(self, track: track):
+    def insertTrackBlocked(self, track_id: str):
         """This method is used to block a track and recalculate the routes of relevant trains.
 
-        :param track: the blocked track
-        :type track: track (as a simulation_object)
+        :param track_id: the id of the blocked track
+        :type track_id: str
         :raises NotImplementedError: This has not yet been implemented.
         """
         raise NotImplementedError()
 
-    def insertTrackNotBlocked(self, track: track):
+    def insertTrackUnblocked(self, track_id: str):
         """This method is used to unblock a track and recalculate the routes of relevant trains.
 
-        :param track: the unblocked track
-        :type track: track (as a simulation_object)
+        :param track_id: the id of the unblocked track
+        :type track_id: str
         :raises NotImplementedError: This has not yet been implemented.
         """
         raise NotImplementedError()
 
-    def insertPlatformBlocked(self, platform: platform):
+    def insertPlatformBlocked(self, platform_id: str):
         """This method is used to block a platform and recalculate the routes and stops of relevant trains.
 
-        :param platform: the blocked platform
-        :type platform: platform (as a simulation_object)
+        :param platform_id: the id of the blocked platform
+        :type platform_id: str
         :raises NotImplementedError: This has not yet been implemented.
         """
         raise NotImplementedError()
 
-    def insertPlatformNotBlocked(self, platform: platform):
+    def insertPlatformUnblocked(self, platform_id: str):
         """This method is used to unblock a platform and recalculate the routes and stops of relevant trains.
 
-        :param platform: the unblocked platform
-        :type platform: platform (as a simulation_object)
+        :param platform_id: the id of the unblocked platform
+        :type platform_id: str
         :raises NotImplementedError: This has not yet been implemented.
         """
         raise NotImplementedError()
 
-    def insertTrackSpeedLimitChanged(self, track: track):
+    def insertTrackSpeedLimitChanged(self, track_id: str):
         """This method is used to notify the interlocking about a changed track speed limit, so that it can recalculate the routing of relevant trains.
 
-        :param track: the track, which speedlimit changed
-        :type track: track (as a simulation_object)
+        :param track_id: the id of the track, which speedlimit changed
+        :type track_id: str
         :raises NotImplementedError: This has not yet been implemented.
         """
         raise NotImplementedError()
 
-    def insertTrainSpeedChanged(self, train: train):
+    def insertTrainSpeedChanged(self, train_id: str):
         """This method is used to notify the interlocking about a changed train speed limit, so that it can recalculate the routing of relevant trains.
 
-        :param train: the train, which speed limit changed
-        :type train: train (as a simulation_object)
+        :param train_id: the id of the train, which speed limit changed
+        :type train_id: str
         :raises NotImplementedError: This has not yet been implemented.
         """
         raise NotImplementedError()
