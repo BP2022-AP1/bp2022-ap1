2259fb0dedd74630fef7d72103abb6ff28959718
CR1337 <captain_2@gmx.de>
2023-04-03 16:55:21 +0200

    style: reformat

--- src/constants.py
+++ src/constants.py
@@ -1,11 +1,11 @@
 from typing import Type
 
 from src.base_model import BaseModel
+from src.implementor.models import Run, SimulationConfiguration, Token
 from src.schedule.regular_schedule_strategy import RegularScheduleStrategy
 from src.schedule.schedule import Schedule
 from src.schedule.schedule_strategy import ScheduleStrategy
 from src.schedule.train_schedule import TrainSchedule, TrainScheduleXSimulationPlatform
-from src.implementor.models import Run, SimulationConfiguration, Token
 
 # Add classes that should be created as tables to this list
 tables: list[Type[BaseModel]] = [
@@ -15,6 +15,6 @@ tables: list[Type[BaseModel]] = [
     Schedule,
     TrainSchedule,
     TrainScheduleXSimulationPlatform,
-    SimulationConfiguration, 
-    Token
+    SimulationConfiguration,
+    Token,
 ]
