642ec6e7151c39db0bc89409c684360b65230d17
MaxLietze <Max.Lietze@student.hpi.de>
2023-04-03 18:05:19 +0200

    fix lint

--- tests/tests_interlocking/test_interlocking.py
+++ tests/tests_interlocking/test_interlocking.py
@@ -2,6 +2,5 @@ from interlocking import test_interlocking
 
 
 def test_from_interlocking():
-    """This test calls the test_01 from the interlocking repository.
-    """
+    """This test calls the test_01 from the interlocking repository."""
     test_interlocking.test_01()
