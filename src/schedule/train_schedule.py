f0bcf54625274cf9b75a5bd10b47357f4a4dcd65
CR1337 <captain_2@gmx.de>
2023-04-03 16:25:12 +0200

    refactor: changed order of classes

--- src/schedule/train_schedule.py
+++ src/schedule/train_schedule.py
@@ -5,31 +5,6 @@ from src.base_model import BaseModel
 from src.schedule.schedule import Schedule
 
 
-class TrainScheduleXSimulationPlatform(BaseModel):
-    """A many-to-many relationship between a train schedule and a simulation platform."""
-
-    class Schema(BaseModel.Schema):
-        """Schema for TrainScheduleXSimulationPlatform."""
-
-        train_schedule_id = marsh.fields.UUID(required=True)
-        simulation_platform_id = marsh.fields.UUID(required=True)
-        index = marsh.fields.Int(required=True)
-
-        def _make(self, data: dict) -> "TrainScheduleXSimulationPlatform":
-            """Constructs a TrainScheduleXSimulationPlatform object from a dictionary.
-
-            :param data: the dictionary
-            :return: an instance of the model
-            """
-            return TrainScheduleXSimulationPlatform(**data)
-
-    train_schedule_id = ForeignKeyField(
-        "TrainSchedule", null=False, backref="platforms"
-    )
-    simulation_platform_id = TextField(null=False)
-    index = IntegerField(null=False)
-
-
 class TrainSchedule(Schedule):
     """A schedule for spawning SUMO trains."""
 
@@ -55,3 +30,28 @@ class TrainSchedule(Schedule):
         """
         # Trains are not implemented yet.
         raise NotImplementedError()
+
+
+class TrainScheduleXSimulationPlatform(BaseModel):
+    """A many-to-many relationship between a train schedule and a simulation platform."""
+
+    class Schema(BaseModel.Schema):
+        """Schema for TrainScheduleXSimulationPlatform."""
+
+        train_schedule_id = marsh.fields.UUID(required=True)
+        simulation_platform_id = marsh.fields.UUID(required=True)
+        index = marsh.fields.Int(required=True)
+
+        def _make(self, data: dict) -> "TrainScheduleXSimulationPlatform":
+            """Constructs a TrainScheduleXSimulationPlatform object from a dictionary.
+
+            :param data: the dictionary
+            :return: an instance of the model
+            """
+            return TrainScheduleXSimulationPlatform(**data)
+
+    train_schedule_id = ForeignKeyField(
+        TrainSchedule, null=False, backref="platforms"
+    )
+    simulation_platform_id = TextField(null=False)
+    index = IntegerField(null=False)
