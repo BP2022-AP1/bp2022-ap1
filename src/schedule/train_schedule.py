55bebab3f797730dfbcda2b6cb826061fbcde163
CR1337 <captain_2@gmx.de>
2023-04-03 14:43:27 +0200

    feat: implemented TrainSchedule

--- src/schedule/train_schedule.py
+++ src/schedule/train_schedule.py
@@ -1,25 +1,55 @@
+import marshmallow as marsh
+from peewee import ForeignKeyField, IntegerField, TextField
+
+from src.base_model import BaseModel
 from src.schedule.schedule import Schedule
+
+
+class TrainScheduleXSimulationPlatform(BaseModel):
+    """A many-to-many relationship between a train schedule and a simulation platform.
+    """
+    class Schema(BaseModel.Schema):
+        """Schema for TrainScheduleXSimulationPlatform.
+        """
+        train_schedule_id = marsh.fields.UUID(required=True)
+        simulation_platform_id = marsh.fields.UUID(required=True)
+        index = marsh.fields.Int(required=True)
+
+        def _make(self, data: dict) -> "TrainScheduleXSimulationPlatform":
+            """Constructs a TrainScheduleXSimulationPlatform object from a dictionary.
+
+            :param data: the dictionary
+            :return: an instance of the model
+            """
+            return TrainScheduleXSimulationPlatform(**data)
+
+    train_schedule_id = ForeignKeyField("TrainSchedule", null=False, backref="platforms")
+    simulation_platform_id = TextField(null=False)
+    index = IntegerField(null=False)
 
 
 class TrainSchedule(Schedule):
     """A schedule for spawning SUMO trains."""
 
-    @classmethod
-    def from_json(cls, json_object: str) -> "TrainSchedule":
-        """Constructs a TrainSchedule from a JSON object.
-
-        :param json_object: The JSON object.
-        :type json_object: str
-        :return: A TrainSchedule.
-        :rtype: TrainSchedule
+    class Schema(Schedule.Schema):
+        """Schema for TrainSchedule.
         """
-        raise NotImplementedError()
+        train_type = marsh.fields.Str(required=True)
 
-    def maybe_spawn(self, tick: int):
-        """Determines whether a train should be spawned at the
-        current tick and spawns it if so.
+        def _make(self, data: dict) -> "TrainSchedule":
+            """Constructs a TrainSchedule object from a dictionary.
 
-        :param tick: The current tick.
-        :type tick: int
+            :param data: the dictionary
+            :return: an instance of the model
+            """
+            return TrainSchedule(**data)
+
+    train_type = TextField(null=False)
+
+    def _spawn(self, traci_wrapper: "ITraCiWrapper"):
+        """Spawns a train.
+
+        :param traci_wrapper: The TraCI wrapper to give the train to.
         """
+        # Trains are not implemented yet.
         raise NotImplementedError()
