396494ea2899059919e4015eb97609e7cf8ff610
Lietze <50149435+Lietze@users.noreply.github.com>
2023-04-03 17:36:25 +0200

    Update src/interlocking_component/route_controller.py
    Co-authored-by: Luke <37272151+SaturnHafen@users.noreply.github.com>

--- src/interlocking_component/route_controller.py
+++ src/interlocking_component/route_controller.py
@@ -5,7 +5,7 @@ class RouteController:
     """
 
     def check_if_new_fahrstrasse_is_needed(
-        self, train_id: train_id, track_segment_id: track_segment_id
+        self, train_id: str, track_segment_id: str
     ):
         """This method should be called when a train enters a new track_segment.
         It then checks if the train is near the end of his fahrstrasse and updates it, if necessary.
