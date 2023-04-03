3d52e333f33baf20b74b5ab287b5b936d5f2a0e1
CR1337 <captain_2@gmx.de>
2023-04-03 16:24:35 +0200

    feat: added tables

--- src/constants.py
+++ src/constants.py
@@ -2,6 +2,17 @@ from typing import Type
 
 from src.base_model import BaseModel
 from src.implementor.models import Run
+from src.schedule.regular_schedule_strategy import RegularScheduleStrategy
+from src.schedule.schedule import Schedule
+from src.schedule.schedule_strategy import ScheduleStrategy
+from src.schedule.train_schedule import TrainSchedule, TrainScheduleXSimulationPlatform
 
 # Add classes that should be created as tables to this list
-tables: list[Type[BaseModel]] = [Run]
+tables: list[Type[BaseModel]] = [
+    Run,
+    ScheduleStrategy,
+    RegularScheduleStrategy,
+    Schedule,
+    TrainSchedule,
+    TrainScheduleXSimulationPlatform
+]
