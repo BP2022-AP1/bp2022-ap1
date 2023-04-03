d71551cf9da39c5dd4f5b1beae59fcccc2477a6c
antonykamp <info@antonykamp.de>
2023-04-03 15:00:14 +0200

    fix: order of tables in constants.py

--- src/constants.py
+++ src/constants.py
@@ -4,4 +4,4 @@ from src.base_model import BaseModel
 from src.implementor.models import Run, SimulationConfiguration, Token
 
 # Add classes that should be created as tables to this list
-tables: list[Type[BaseModel]] = [SimulationConfiguration, Token, Run]
+tables: list[Type[BaseModel]] = [Run, SimulationConfiguration, Token]
