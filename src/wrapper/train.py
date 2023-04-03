8a12037b3a2db5efe7a5b5719e85d3906ecfc67f
Luke <37272151+SaturnHafen@users.noreply.github.com>
2023-04-03 11:01:20 +0200

    update: properties

--- src/wrapper/train.py
+++ src/wrapper/train.py
@@ -1,23 +1,27 @@
 from simualtion_object import SimulationObject
 from traci import constants, vehicle
+from enum import Enum
+from typing import List, Protocol, Tuple
 
 
 class Train(SimulationObject):
-    _position = None
+    """A train driving around in the simulation."""
+
+    _position: Tuple(str, str) = None
     _route: str = None
-    _track_id: str = None
+    _track: Track = None
     _vehicle_type: str = None
     _speed: float = None
     _max_speed: float = None
-    timetable: object = None
+    timetable: List[Platform] = None
 
     @property
-    def track_id(self) -> str:
+    def track(self) -> Track:
         """Returns the current track the train is on
 
         :return: _description_
         """
-        return self._track_id
+        return self._track
 
     @property
     def position(self):
@@ -54,13 +58,32 @@ class Train(SimulationObject):
         vehicle.setMaxSpeed(self.traci_id, speed)
         self._max_speed = speed
 
-    def __init__(self, from_traci: bool = False, *args, **kwargs):
+    def __init__(
+        self,
+        identifier: str = None,
+        timetable: List[Platform] = None,
+        train_type: str = None,
+        from_simulator: bool = False,
+    ):
+        """Creates a new train from the given parameters.
+        When initializing manually, `timetable` and `train_type` are mandatory
+        :param identifier: 
+        :param timetable: The stations which the train should drive along,
+        in the correct order. Mandatory when initializing the train yourself.
+        :param train_type: The type of the train (as a SUMO VEHICLE_TYPE).
+        Mandatory when initializing the train yourself.
+        :param from_simulator: Specifies if train is created by the simulation or manually.
+        You probably don't need to touch this.
+        """
         SimulationObject.__init__(self, *args, **kwargs)
 
-    def _add_to_simulation(self, route_id: str, vehicle_type: str):
+    def _add_to_simulation(self, timetable: List[Platform], train_type: str):
         vehicle.add("asdf", route_id, vehicle_type)
 
     def update(self, updates: dict):
+        """Gets called whenever a simualtion tick has happened.
+        :param updates: The updated values for the synchronized properties
+        """
         self._position = updates[constants.VAR_POSITION]
         self._track_id = updates[constants.VAR_ROAD_ID]
         self._route = updates[constants.VAR_ROUTE]
@@ -68,6 +91,10 @@ class Train(SimulationObject):
         self._max_speed = updates[constants.VAR_MAXSPEED]
 
     def add_subscriptions(self) -> int:
+        """Gets called when this object is created to allow
+        specification of simulator-synchronized properties.
+        :return: The synchronized properties (see <https://sumo.dlr.de/pydoc/traci.constants.html>)
+        """
         return (
             constants.VAR_POSITION
             + constants.VAR_ROUTE
@@ -78,16 +105,50 @@ class Train(SimulationObject):
 
 
 class Node(SimulationObject):
+    """A point somewhere in the simulation where `ISImulationTrack`s meet"""
+
     pass
 
 
 class Signal(Node):
-    pass
+    """A signal in the simulation which can either show stop or go
+    (See `ISimulationSignal.State`)
+    """
+
+    class State(Enum):
+        """The possible states of the signal"""
+
+        HALT = 1
+        GO = 2
+
+    state: State
 
 
 class Switch(Node):
-    pass
+    """A switch in the simulation which can point either left or right
+    (see `ISimulationSwitch.State`)
+    """
+
+    class State(Enum):
+        """The possible states of the switch"""
+
+        LEFT = 1
+        RIGHT = 2
+
+    state: State
 
 
 class Track(SimulationObject):
-    pass
+    """A track in the simulation where trains can drive along"""
+
+    blocked: bool
+    speed_limit: float
+
+
+class Platform(SimulationObject):
+    """A platform where trains can arrive, load and unload passengers and depart"""
+
+    track: Track
+    station_name: str
+    platform_number: int
+    blocked: bool
