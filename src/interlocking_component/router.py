84f7c898cd44a5bdaaa896f6f9dfc6f26e94855e
MaxLietze <Max.Lietze@student.hpi.de>
2023-04-04 14:21:02 +0200

    implemeted review feedback

--- src/interlocking_component/router.py
+++ src/interlocking_component/router.py
@@ -1,12 +1,12 @@
 class Router:
     """This class will calculate routes for trains from track segment to track segment."""
 
-    def get_route(self, start_track_segment_id: str, end_track_segment_id: str):
+    def get_route(self, start_track_segment: "Track", end_track_segment: "Track"):
         """This method returns a route from a start track_segment to an end track_segment.
 
-        :param start_track_segment_id: The id of the track segment where the route will begin.
-        :type start_track_segment_id: str
-        :param end_track_segment_id: The id of the track segment where the route will end.
-        :type end_track_segment_id: str
+        :param start_track_segment: The track segment where the route will begin.
+        :type start_track_segment: Track
+        :param end_track_segment_id: The track segment where the route will end.
+        :type end_track_segment: Track
         """
         raise NotImplementedError()
