177c38526d949b34096ef02813713cd8dabec910
Luke <37272151+SaturnHafen@users.noreply.github.com>
2023-04-04 16:42:56 +0200

    feat: track tests

--- tests/wrapper/test_simulation_objects.py
+++ tests/wrapper/test_simulation_objects.py
@@ -1,7 +1,7 @@
 import pytest
-from traci import trafficlight
+from traci import trafficlight, edge
 
-from src.wrapper.simulation_objects import Signal
+from src.wrapper.simulation_objects import Signal, Track
 
 
 class TestSignal:
@@ -28,3 +28,32 @@ class TestSignal:
         signal.state = Signal.State.GO
 
         assert signal.state == Signal.State.GO
+
+class TestTrack:
+    """Tests for the track component"""
+
+    @pytest.fixture
+    def track(self):
+        return Track("track")
+
+    @pytest.fixture
+    def speed_update(self, monkeypatch):
+        def set_max_speed(identifier: str, speed: float) -> None:
+            assert identifier == "track"
+            assert speed == 100
+
+        monkeypatch.setattr(edge, "setMaxSpeed", set_max_speed)
+
+    def test_update_speed(self, track, speed_update):
+        # pylint: disable=unused-argument
+        track.max_speed = 100
+
+        assert track.max_speed == 100
+
+    def test_default_blocked(self, track):
+        assert not track.blocked
+
+    def test_update_blocked(self, track):
+        track.blocked = True
+
+        assert track.blocked
