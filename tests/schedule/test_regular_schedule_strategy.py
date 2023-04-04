a54972c82c54a01cf51b1f5e11296d2fa27650ca
CR1337 <captain_2@gmx.de>
2023-04-04 10:55:45 +0200

    refactor: use fixtures and parametrization

--- tests/schedule/test_regular_schedule_strategy.py
+++ tests/schedule/test_regular_schedule_strategy.py
@@ -1,46 +1,57 @@
+import pytest
+
 from src.schedule.regular_schedule_strategy import RegularScheduleStrategy
 
 
-def test_not_spawning_before_start_tick():
-    strategy = RegularScheduleStrategy(start_tick=1000, frequency=100)
-    assert not strategy.should_spawn(tick=900)
+@pytest.fixture
+def regular_strategy() -> RegularScheduleStrategy:
+    return RegularScheduleStrategy(start_tick=1000, frequency=100)
 
 
-def test_spawning_at_start_tick():
-    strategy = RegularScheduleStrategy(start_tick=10, frequency=100)
-    assert strategy.should_spawn(tick=10)
+@pytest.mark.parametrize(
+    "strategy,tick", (("regular_strategy", tick) for tick in [0, 900])
+)
+def test_not_spawning_before_start_tick(strategy, tick, request):
+    strategy = request.getfixturevalue(strategy)
+    assert not strategy.should_spawn(tick=tick)
 
 
-def test_spawning_regularly():
-    strategy = RegularScheduleStrategy(start_tick=10, frequency=100)
-    assert strategy.should_spawn(tick=110)
-    assert strategy.should_spawn(tick=210)
-    assert strategy.should_spawn(tick=310)
+@pytest.mark.parametrize("strategy,tick", [("regular_strategy", 1000)])
+def test_spawning_at_start_tick(strategy, tick, request):
+    strategy = request.getfixturevalue(strategy)
+    assert strategy.should_spawn(tick=tick)
 
 
-def test_not_spawning_in_between():
-    strategy = RegularScheduleStrategy(start_tick=10, frequency=100)
-    assert not strategy.should_spawn(tick=115)
-    assert not strategy.should_spawn(tick=216)
-    assert not strategy.should_spawn(tick=317)
+@pytest.mark.parametrize(
+    "strategy,tick", (("regular_strategy", tick) for tick in [1100, 1200, 1300])
+)
+def test_spawning_regularly(strategy, tick, request):
+    strategy = request.getfixturevalue(strategy)
+    assert strategy.should_spawn(tick=tick)
 
 
-def test_de_serialization():
-    strategy = RegularScheduleStrategy(start_tick=10, frequency=100)
-    serialized = strategy.to_dict()
+@pytest.mark.parametrize(
+    "strategy,tick", (("regular_strategy", tick) for tick in [1105, 1250, 1355])
+)
+def test_not_spawning_in_between(strategy, tick, request):
+    strategy = request.getfixturevalue(strategy)
+    assert not strategy.should_spawn(tick=tick)
+
+
+def test_de_serialization(regular_strategy):
+    serialized = regular_strategy.to_dict()
     deserialized = RegularScheduleStrategy.from_dict(serialized)
-    assert strategy.start_tick == deserialized.start_tick
-    assert strategy.frequency == deserialized.frequency
+    assert regular_strategy.start_tick == deserialized.start_tick
+    assert regular_strategy.frequency == deserialized.frequency
 
 
-def test_database_interaction():
-    strategy = RegularScheduleStrategy(start_tick=10, frequency=100)
-    strategy.save(force_insert=True)
-    id_ = strategy.id
+def test_database_interaction(regular_strategy):
+    regular_strategy.save(force_insert=True)
+    id_ = regular_strategy.id
     db_strategy = (
         RegularScheduleStrategy.select()
         .where(RegularScheduleStrategy.id == id_)
         .first()
     )
-    assert db_strategy.start_tick == strategy.start_tick
-    assert db_strategy.frequency == strategy.frequency
+    assert db_strategy.start_tick == regular_strategy.start_tick
+    assert db_strategy.frequency == regular_strategy.frequency
