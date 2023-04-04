4854557ff5f8038f71e5be5ebe255e4110d6b734
Anton Persitzky <82519211+instant-sky@users.noreply.github.com>
2023-04-04 11:59:00 +0200

    style: add type hints in test methods

--- tests/test_table_basic.py
+++ tests/test_table_basic.py
@@ -5,6 +5,7 @@ import peewee
 import pytest
 
 from src.implementor.models import Run, SimulationConfiguration, Token
+from src.base_model import BaseModel
 
 
 @pytest.mark.parametrize(
@@ -19,14 +20,14 @@ from src.implementor.models import Run, SimulationConfiguration, Token
 class TestFailingDict:
     """Test that a object of a class cannot be created or deserialized with invalid data."""
 
-    def test_create(self, table_class, object_as_dict):
+    def test_create(self, table_class: BaseModel, object_as_dict: dict):
         """Test that an object of a class cannot be created."""
         with pytest.raises(peewee.IntegrityError):
             table_class.create(
                 **object_as_dict,
             )
 
-    def test_deserialization(self, table_class, object_as_dict):
+    def test_deserialization(self, table_class: BaseModel, object_as_dict: dict):
         """Test that an object of a class cannot be deserialized."""
         with pytest.raises(marsh.exceptions.ValidationError):
             table_class.Schema().load(object_as_dict)
@@ -51,14 +52,14 @@ class TestFailingDict:
 class TestCorrectFilledDict:
     """Test that a object of a class can be created and deserialized with valid data."""
 
-    def test_create(self, table_class, object_as_dict):
+    def test_create(self, table_class: BaseModel, object_as_dict: dict):
         """Test that a object of a class can be created."""
         obj = table_class.create(
             **object_as_dict,
         )
         assert table_class.select().where(table_class.id == obj.id).first() == obj
 
-    def test_serialization(self, table_class, object_as_dict):
+    def test_serialization(self, table_class: BaseModel, object_as_dict: dict):
         """Test that an object of a class can be serialized."""
         obj = table_class.create(
             **object_as_dict,
@@ -75,7 +76,7 @@ class TestCorrectFilledDict:
         for key in none_fields:
             assert getattr(obj, key) is None
 
-    def test_deserialization_full_dict(self, table_class, object_as_dict):
+    def test_deserialization_full_dict(self, table_class: BaseModel, object_as_dict: dict):
         """Test that an object of a class can be deserialized."""
         obj = table_class.Schema().load(
             object_as_dict,
