4ac6c9d58d5d93f98c234714bf42c1e47e0c6dce
Luke <37272151+SaturnHafen@users.noreply.github.com>
2023-04-06 15:20:44 +0200

    feat: query switches

--- src/wrapper/simulation_object_updating_component.py
+++ src/wrapper/simulation_object_updating_component.py
@@ -2,8 +2,8 @@ from typing import List
 
 import traci
 
-import src.wrapper.simulation_objects
 from src.component import Component
+from src.wrapper.simulation_objects import Platform, Switch
 
 
 class SimulationObjectUpdatingComponent(Component):
@@ -15,16 +15,20 @@ class SimulationObjectUpdatingComponent(Component):
     _simulation_objects = None
 
     @property
+    def switches(self) -> List[Switch]:
+        """Returns all switches in the simulation
+
+        :return: The switches in the simulation
+        """
+        return [x for x in self._simulation_objects if isinstance(x, Switch)]
+
+    @property
     def platforms(self) -> List["Platform"]:
         """Returns all platforms in the simulation
 
         :return: The platforms in the simulation
         """
-        return [
-            x
-            for x in self._simulation_objects
-            if isinstance(x, src.wrapper.simulation_objects.Platform)
-        ]
+        return [x for x in self._simulation_objects if isinstance(x, Platform)]
 
     def __init__(self, *args, **kwargs):
         Component.__init__(self, *args, **kwargs)
