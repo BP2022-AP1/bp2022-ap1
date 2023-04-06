0d097bf3f22361eb44627d03d69961b53936143f
CR1337 <captain_2@gmx.de>
2023-04-06 17:30:37 +0200

    feat: tests for date fields in BaseModel

--- tests/db/test_db.py
+++ tests/db/test_db.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from uuid import UUID
 
 import marshmallow as marsh
@@ -28,6 +29,8 @@ class ModelTest(BaseModel):
 class TestDB:
     """Test the database connection and the serialization/deserialization"""
 
+    TEST_DATETIME_STRING: str = "2023-04-06 16:42:01"
+
     @recreate_db_setup
     def setup_method(self):
         db.create_tables([ModelTest])
@@ -37,17 +40,39 @@ class TestDB:
 
     def test_db_connection_workflow(self):
         # I just wanted to test if the db is working
-        ModelTest.create(test_value=1).save()
+        ModelTest(test_value=1).save()
         test_obj = ModelTest.select().where(ModelTest.test_value == 1).first()
         assert test_obj.test_value == 1
 
     def test_serialization(self):
         # I just wanted to test if the serialization is working
         test_obj = ModelTest(test_value=1)
-        assert test_obj.to_dict() == {"id": str(test_obj.id), "test_value": 1}
+        assert {
+            "id": str(test_obj.id),
+            "test_value": 1,
+        }.items() <= test_obj.to_dict().items()
 
     def test_deserialization(self):
         # I just wanted to test if the deserialization is working
         test_obj = ModelTest.Schema().load({"test_value": 1})
         assert test_obj.test_value == 1
         assert isinstance(test_obj.id, UUID)
+
+    def test_created_at(self):
+        start_datetime = datetime.now()
+        test_obj = ModelTest(test_value=1)
+        end_datetime = datetime.now()
+        created_at = test_obj.created_at
+        assert start_datetime <= created_at <= end_datetime
+
+    def test_updated_at(self):
+        start_datetime = datetime.now()
+        test_obj = ModelTest(test_value=1)
+        mid_datetime = datetime.now()
+        test_obj.save()
+        end_datetime = datetime.now()
+        created_at = test_obj.created_at
+        updated_at = test_obj.updated_at
+        assert (
+            start_datetime <= created_at <= mid_datetime <= updated_at <= end_datetime
+        )
