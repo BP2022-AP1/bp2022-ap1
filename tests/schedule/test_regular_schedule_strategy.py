c63c8c3a9038b46fe05c6d8719063ae5612eb003
CR1337 <captain_2@gmx.de>
2023-04-07 13:12:33 +0200

    fix: merging bugs in tests

--- tests/schedule/test_regular_schedule_strategy.py
+++ tests/schedule/test_regular_schedule_strategy.py
@@ -55,28 +55,3 @@ class TestRegularScheduleStrategy:
     ):
         strategy = request.getfixturevalue(strategy)
         assert not strategy.should_spawn(tick=tick)
-
-    def test_de_serialization(
-        self, regular_strategy: Callable[[None], RegularScheduleStrategy]
-    ):
-        serialized = regular_strategy.to_dict()
-        del serialized["id"]
-        del serialized["created_at"]
-        del serialized["updated_at"]
-        deserialized = RegularScheduleStrategy.from_dict(serialized)
-        assert regular_strategy.start_tick == deserialized.start_tick
-        assert regular_strategy.frequency == deserialized.frequency
-
-    @recreate_db
-    def test_database_interaction(
-        self, regular_strategy: Callable[[None], RegularScheduleStrategy]
-    ):
-        regular_strategy.save(force_insert=True)
-        id_ = regular_strategy.id
-        db_strategy = (
-            RegularScheduleStrategy.select()
-            .where(RegularScheduleStrategy.id == id_)
-            .first()
-        )
-        assert db_strategy.start_tick == regular_strategy.start_tick
-        assert db_strategy.frequency == regular_strategy.frequency
