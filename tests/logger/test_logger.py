0d6ba650ed794787ac475271563e2560dcf55e7c
Lucas Reisener <lucasreisener@gmail.com>
2023-04-12 15:47:18 +0200

    Feature/164 spawn train log function (#218)
    * create log entry schema & tests
    
    * fix lint by adding ignore statements
    
    * reformat with black
    
    * moving fixtures to conftest.py
    
    * implement log spawn train function & tests
    
    * added tick to trainSpawnLog

---
+++ tests/logger/test_logger.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from freezegun import freeze_time
+
+from src.logger.log_entry import TrainSpawnLogEntry
+from src.logger.logger import Logger
+
+
+class TestLogger:
+    """Class for testing logger functions."""
+
+    @freeze_time()
+    def test_spawn_train(self, run, tick, train_id):
+        logger = Logger(run_id=run.id)
+        logger.spawn_train(tick=tick, train_id=train_id)
+        log_entry = (
+            TrainSpawnLogEntry.select()
+            .where(TrainSpawnLogEntry.train_id == train_id)
+            .first()
+        )
+        assert log_entry.timestamp == datetime.now()
+        assert log_entry.tick == tick
+        assert log_entry.message == f"Train with ID {train_id} spawned"
+        assert log_entry.run_id.id == run.id
+        assert log_entry.train_id == train_id
