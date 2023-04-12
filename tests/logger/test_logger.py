5876a456d27f4368467e89fcec9467b7197ff107
Lucas Reisener <lucasreisener@gmail.com>
2023-04-12 15:53:46 +0200

    Feature/167 remove train log function (#276)
    * create log entry schema & tests
    
    * fix lint by adding ignore statements
    
    * reformat with black
    
    * moving fixtures to conftest.py
    
    * implement log spawn train function & tests
    
    * added tick to trainSpawnLog
    
    * implement remove train log entry
    
    * fix lint

--- tests/logger/test_logger.py
+++ tests/logger/test_logger.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from freezegun import freeze_time
 
-from src.logger.log_entry import TrainSpawnLogEntry
+from src.logger.log_entry import TrainRemoveLogEntry, TrainSpawnLogEntry
 from src.logger.logger import Logger
 
 
@@ -21,5 +21,20 @@ class TestLogger:
         assert log_entry.timestamp == datetime.now()
         assert log_entry.tick == tick
         assert log_entry.message == f"Train with ID {train_id} spawned"
+        assert log_entry.run_id.id == run.id
+        assert log_entry.train_id == train_id
+
+    @freeze_time()
+    def test_remove_train(self, run, tick, train_id):
+        logger = Logger(run_id=run.id)
+        logger.remove_train(tick=tick, train_id=train_id)
+        log_entry = (
+            TrainRemoveLogEntry.select()
+            .where(TrainRemoveLogEntry.train_id == train_id)
+            .first()
+        )
+        assert log_entry.timestamp == datetime.now()
+        assert log_entry.tick == tick
+        assert log_entry.message == f"Train with ID {train_id} removed"
         assert log_entry.run_id.id == run.id
         assert log_entry.train_id == train_id
