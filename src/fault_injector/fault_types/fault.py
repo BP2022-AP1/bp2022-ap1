2686cb9867696b67ffc142d2b97c0dd19606fd02
Anton Persitzky <82519211+instant-sky@users.noreply.github.com>
2023-04-13 09:54:57 +0200

    fix: lint

--- src/fault_injector/fault_types/fault.py
+++ src/fault_injector/fault_types/fault.py
@@ -29,7 +29,7 @@ class Fault(ABC):
     @abstractmethod
     def resolve_fault(self, tick: int):
         """resolves the previously injected fault
-        
+
         :param tick: the simulation tick in which resolve_fault was called
         :type tick: Integer
         """
