c4c9ec2745f3fdcf88d5bc8c6752d3a6718bb118
Anton <82519211+instant-sky@users.noreply.github.com>
2023-04-13 15:34:04 +0200

    Rename `TrainCancelledFault` to `ScheduleBlockedFault` (#291)
    * refact: rename train_cancelled_fault.py
    
    * refact: rename train_cancelled_fault_configuration.py
    
    * refact: rename in constants.py
    
    * refact: rename in log_entry.py
    
    * refact: rename in logger.py
    
    * refact: rename in test_table_fault_configurations.py
    
    * refact: rename in conftest.py
    
    * refact: rename in test_log_entry.py
    
    * refact: cleanup after renaming

--- src/fault_injector/fault_types/schedule_blocked_fault.py
+++ src/fault_injector/fault_types/schedule_blocked_fault.py
@@ -1,21 +1,21 @@
 from src.fault_injector.fault_types.fault import Fault
 
 
-class TrainCancelledFault(Fault):
+class ScheduleBlockedFault(Fault):
     """A fault that blocks a platform"""
 
     def inject_fault(self, tick: int):
-        """inject TrainCancelledFault into the given component
+        """inject ScheduleBlockedFault into the given component
 
         :param tick: the simulation tick in which inject_fault was called
         :type tick: Integer
         """
-        # - get train by id
-        # - mark train as cancelled
+        # - get schedule by id
+        # - mark schedule as blocked
         raise NotImplementedError()
 
     def resolve_fault(self, tick: int):
-        """resolves the previously injected TrainCancelledFault
+        """resolves the previously injected ScheduleBlockedFault
 
         :param tick: the simulation tick in which resolve_fault was called
         :type tick: Integer
