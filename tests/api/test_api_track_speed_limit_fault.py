60e84b57133f8d64d2c57f9b23bb271cd2e3b646
antonykamp <info@antonykamp.de>
2023-04-14 10:18:52 +0200

    fix: typo "track-split-limit-fault" in code

--- tests/api/test_api_track_speed_limit_fault.py
+++ tests/api/test_api_track_speed_limit_fault.py
@@ -17,20 +17,20 @@ class TestApiTrackSpeedLimitFault:
     def test_get_single(self, client):
         object_id = uuid.uuid4()
         response = client.get(
-            f"/component/fault-injection/track-split-limit-fault/{object_id}"
+            f"/component/fault-injection/track-speed-limit-fault/{object_id}"
         )
         assert response.status_code == 501
 
     def test_update(self, client):
         object_id = uuid.uuid4()
         response = client.put(
-            f"/component/fault-injection/track-split-limit-fault/{object_id}"
+            f"/component/fault-injection/track-speed-limit-fault/{object_id}"
         )
         assert response.status_code == 501
 
     def test_delete(self, client):
         object_id = uuid.uuid4()
         response = client.delete(
-            f"/component/fault-injection/track-split-limit-fault/{object_id}"
+            f"/component/fault-injection/track-speed-limit-fault/{object_id}"
         )
         assert response.status_code == 501
