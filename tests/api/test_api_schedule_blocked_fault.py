4d394d450b02e34a55483d0896ea8863bfa1adb8
Antony Kamp <antony.kamp@student.hpi.de>
2023-04-14 11:28:49 +0200

    Fix: Typo "schedule-blockedfault" to "schedule-blocked-fault" (#304)
    * fix: typo "schedule-blockedfault" in openApi3 definition and collection
    
    * fix: missing typo in PUT
    
    * fix: typo "schedule-blockedfault" in code
    
    * fix: replace blockedfault in autogenerated files

--- tests/api/test_api_schedule_blocked_fault.py
+++ tests/api/test_api_schedule_blocked_fault.py
@@ -17,20 +17,20 @@ class TestApiScheduleBlockedFault:
     def test_get_single(self, client):
         object_id = uuid.uuid4()
         response = client.get(
-            f"/component/fault-injection/schedule-blockedfault/{object_id}"
+            f"/component/fault-injection/schedule-blocked-fault/{object_id}"
         )
         assert response.status_code == 501
 
     def test_update(self, client):
         object_id = uuid.uuid4()
         response = client.put(
-            f"/component/fault-injection/schedule-blockedfault/{object_id}"
+            f"/component/fault-injection/schedule-blocked-fault/{object_id}"
         )
         assert response.status_code == 501
 
     def test_delete(self, client):
         object_id = uuid.uuid4()
         response = client.delete(
-            f"/component/fault-injection/schedule-blockedfault/{object_id}"
+            f"/component/fault-injection/schedule-blocked-fault/{object_id}"
         )
         assert response.status_code == 501
