58d7205636c73ad823511885da2ef6045b435322
Lietze <50149435+Lietze@users.noreply.github.com>
2023-04-17 14:55:47 +0200

    added tests that are based on fault_config tests (#313)
    * added tests that are based on fault_config tests
    
    * fix lint
    
    * this time realy fixed lint
    
    * changed to comments
    
    * fix lint

--- tests/interlocking_component/test_interlocking_configuration.py
+++ tests/interlocking_component/test_interlocking_configuration.py
@@ -1,8 +1,51 @@
-from src.interlocking_component.route_controller import InterlockingConfiguration
+from uuid import UUID
+
+import pytest
+
+from src.interlocking_component.interlocking_configuration import (
+    InterlockingConfiguration,
+)
+from tests.decorators import recreate_db_setup
 
 
-def test_default_true():
-    """This test if an InterlockingConfiguration Object can be initialized
-    and if the default for dynamicRouting is false."""
-    interlocking_configuration = InterlockingConfiguration()
-    assert not interlocking_configuration.dynamicRouting
+@pytest.mark.parametrize(
+    "object_as_dict",
+    [
+        {
+            "dynamicRouting": True,
+        },
+    ],
+)
+class TestCorrectFilledDict:
+    """Test that a object of a class can be created and deserialized with valid data."""
+
+    @recreate_db_setup
+    def setup_method(self):
+        pass
+
+    def test_create(self, object_as_dict: dict):
+        """Test that a object of a class can be created."""
+        obj = InterlockingConfiguration.create(
+            **object_as_dict,
+        )
+        assert (
+            InterlockingConfiguration.select()
+            .where(InterlockingConfiguration.id == obj.id)
+            .first()
+            == obj
+        )
+
+    def test_serialization(self, object_as_dict: dict):
+        """Test that an object of a class can be serialized."""
+        obj_dict = InterlockingConfiguration.create(**object_as_dict).to_dict()
+
+        for key in object_as_dict.keys():
+            assert obj_dict[key] == object_as_dict[key]
+
+    def test_deserialization_full_dict(self, object_as_dict: dict):
+        """Test that an object of a class can be deserialized."""
+        obj = InterlockingConfiguration.from_dict(object_as_dict)
+        assert isinstance(obj, InterlockingConfiguration)
+        assert isinstance(obj.id, UUID)
+        for key in object_as_dict.keys():
+            assert getattr(obj, key) == object_as_dict[key]
