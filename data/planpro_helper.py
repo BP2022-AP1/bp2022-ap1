1e0c66c9b2b48850ca499f81a2cf3a814bbf7473
MaxLietze <Max.Lietze@student.hpi.de>
2023-04-17 17:26:14 +0200

    fix lint and add __init__.py

--- data/planpro_helper.py
+++ data/planpro_helper.py
@@ -1,7 +1,7 @@
-
 from orm_importer.importer import ORMImporter
 from planproexporter import Generator
 from railwayroutegenerator.routegenerator import RouteGenerator
+
 
 def generate_plan_pro():
     polygon = "51.559286255426606 14.22958373936126 51.52956887166332 14.249153136333918 51.547102337669735 14.353866576275324 51.57915827365655 14.286231993755793"
@@ -15,8 +15,10 @@ def generate_plan_pro():
 
     # Write PlanPro
     generator = Generator()
-    generator.generate(topology, "BP2022-AP1", "BP2022-AP1", "data/planpro/test_example")
-    
+    generator.generate(
+        topology, "BP2022-AP1", "BP2022-AP1", "data/planpro/test_example"
+    )
+
 
 if __name__ == "__main__":
     generate_plan_pro()
