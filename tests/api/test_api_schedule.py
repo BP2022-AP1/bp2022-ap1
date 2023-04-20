f2092a46ec3e328d08c716569ebfa998f78aef23
Antony Kamp <antony.kamp@student.hpi.de>
2023-04-20 12:27:46 +0200

    Fix: Remove PUT routes from definition, collection, code and tests (#349)
    * fix: remove put routes in openAPI3
    
    * fix: remove put requests from collection
    
    * fix: remove put endpoints
    
    * fix: remove update functions in implementor
    
    * fix: remove put tests

--- tests/api/test_api_schedule.py
+++ tests/api/test_api_schedule.py
@@ -19,11 +19,6 @@ class TestApiSchedule:
         response = client.get(f"/schedule/{object_id}")
         assert response.status_code == 501
 
-    def test_update(self, client):
-        object_id = uuid.uuid4()
-        response = client.put(f"/schedule/{object_id}")
-        assert response.status_code == 501
-
     def test_delete(self, client):
         object_id = uuid.uuid4()
         response = client.delete(f"/schedule/{object_id}")
