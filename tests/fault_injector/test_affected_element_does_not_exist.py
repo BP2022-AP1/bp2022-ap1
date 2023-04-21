0075cf44e8a428d37d0d4fdf2b86a03650525850
Anton Persitzky <anton.persitzky@student.hpi.uni-potsdam.de>
2023-04-21 11:21:50 +0200

    fix: lint

--- tests/fault_injector/test_affected_element_does_not_exist.py
+++ tests/fault_injector/test_affected_element_does_not_exist.py
@@ -5,36 +5,41 @@ import peewee
 import pytest
 
 from src.base_model import BaseModel
+from src.fault_injector.fault_configurations.fault_configuration import (
+    FaultConfiguration,
+)
 from src.fault_injector.fault_configurations.platform_blocked_fault_configuration import (
     PlatformBlockedFaultConfiguration,
 )
-from src.fault_injector.fault_types.platform_blocked_fault import PlatformBlockedFault
 from src.fault_injector.fault_configurations.schedule_blocked_fault_configuration import (
     ScheduleBlockedFaultConfiguration,
 )
-from src.fault_injector.fault_types.schedule_blocked_fault import ScheduleBlockedFault
 from src.fault_injector.fault_configurations.track_blocked_fault_configuration import (
     TrackBlockedFaultConfiguration,
 )
-from src.fault_injector.fault_types.track_blocked_fault import TrackBlockedFault
 from src.fault_injector.fault_configurations.track_speed_limit_fault_configuration import (
     TrackSpeedLimitFaultConfiguration,
 )
-from src.fault_injector.fault_types.track_speed_limit_fault import TrackSpeedLimitFault
 from src.fault_injector.fault_configurations.train_prio_fault_configuration import (
     TrainPrioFaultConfiguration,
 )
-from src.fault_injector.fault_types.train_prio_fault import TrainPrioFault
 from src.fault_injector.fault_configurations.train_speed_fault_configuration import (
     TrainSpeedFaultConfiguration,
 )
-from src.fault_injector.fault_types.train_speed_fault import TrainSpeedFault
-from tests.decorators import recreate_db_setup
-from src.fault_injector.fault_configurations.fault_configuration import FaultConfiguration
 from src.fault_injector.fault_types.fault import Fault
-from src.logger.logger import Logger
-from src.wrapper.simulation_object_updating_component import SimulationObjectUpdatingComponent
+from src.fault_injector.fault_types.platform_blocked_fault import PlatformBlockedFault
+from src.fault_injector.fault_types.schedule_blocked_fault import ScheduleBlockedFault
+from src.fault_injector.fault_types.track_blocked_fault import TrackBlockedFault
+from src.fault_injector.fault_types.track_speed_limit_fault import TrackSpeedLimitFault
+from src.fault_injector.fault_types.train_prio_fault import TrainPrioFault
+from src.fault_injector.fault_types.train_speed_fault import TrainSpeedFault
 from src.interlocking_component.route_controller import IInterlockingDisruptor
+from src.logger.logger import Logger
+from src.wrapper.simulation_object_updating_component import (
+    SimulationObjectUpdatingComponent,
+)
+from tests.decorators import recreate_db_setup
+
 
 @pytest.mark.parametrize(
     "table_class, fault_type, object_as_dict",
@@ -104,7 +109,6 @@ from src.interlocking_component.route_controller import IInterlockingDisruptor
         ],
     ],
 )
-
 class TestAffectedElementDoesNotExist:
     """Test cases where the requested element for injecting a fault does not exist (in the simulation)"""
 
@@ -115,11 +119,23 @@ class TestAffectedElementDoesNotExist:
     @pytest.fixture
     def fault_configuration(self, table_class: BaseModel, object_as_dict: dict):
         return table_class.create(**object_as_dict)
-    
+
     @pytest.fixture
-    def fault(self, fault_configuration: FaultConfiguration, logger: Logger, simulation_object_updater: SimulationObjectUpdatingComponent, interlocking: IInterlockingDisruptor, fault_type: Fault):
-        return fault_type(configuration=fault_configuration, logger=logger, simulation_object_updater=simulation_object_updater, interlocking=interlocking)
-    
+    def fault(
+        self,
+        fault_configuration: FaultConfiguration,
+        logger: Logger,
+        simulation_object_updater: SimulationObjectUpdatingComponent,
+        interlocking: IInterlockingDisruptor,
+        fault_type: Fault,
+    ):
+        return fault_type(
+            configuration=fault_configuration,
+            logger=logger,
+            simulation_object_updater=simulation_object_updater,
+            interlocking=interlocking,
+        )
+
     def test_injection(self, tick, fault: Fault):
         with pytest.raises(ValueError):
             fault.inject_fault(tick)
