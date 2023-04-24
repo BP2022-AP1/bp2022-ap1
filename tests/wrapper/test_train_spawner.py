ba40a700906ef5b5b099a44b930a748cc6a5bf37
Luke <37272151+SaturnHafen@users.noreply.github.com>
2023-04-24 15:54:33 +0200

    fet: more asserts

--- tests/wrapper/test_train_spawner.py
+++ tests/wrapper/test_train_spawner.py
@@ -7,10 +7,16 @@ from src.wrapper.train_spawner import TrainSpawner
 
 
 def test_spawn_train(souc: SimulationObjectUpdatingComponent, spawner: TrainSpawner):
-    assert spawner.spawn_train("test-train-1", ["1", "2"], "test-cargo")
+    assert spawner.spawn_train("test-train-1", ["station-1", "station-2"], "test-cargo")
+
+    assert len(souc.trains) == 1
 
 
 def test_spawn_train_no_route(
     souc: SimulationObjectUpdatingComponent, spawner2: TrainSpawner
 ):
-    assert not spawner2.spawn_train("test-train-1", ["1", "2"], "test-cargo")
+    assert not spawner2.spawn_train(
+        "test-train-2", ["station-2", "station-0"], "test-cargo"
+    )
+
+    assert len(souc.trains) == 0
