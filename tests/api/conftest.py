6b0f83cd6003913c613d5522f552e2137aa975ff
antonykamp <info@antonykamp.de>
2023-04-27 13:38:13 +0200

    feat: add tests for no, invalid and valid token

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -1,6 +1,9 @@
+import hashlib
+
 import pytest
 
 from src import create_app
+from src.implementor.models import Token
 
 
 @pytest.fixture(name="app")
@@ -17,3 +20,29 @@ def fixture_app():
 @pytest.fixture(name="client")
 def fixture_client(app):
     return app.test_client()
+
+
+@pytest.fixture(name="clear_admin_token")
+def fixture_clear_hashed_token():
+    return "token"
+
+
+@pytest.fixture(name="admin_token", autouse=True)
+def fixture_admin_token(clear_admin_token):
+    hashed_token = hashlib.sha256(clear_admin_token.encode()).hexdigest()
+    name = "admin"
+    permission = "admin"
+    return Token.create(name=name, permission=permission, hashedToken=hashed_token)
+
+
+@pytest.fixture(name="clear_token")
+def fixture_not_hashed_token():
+    return "token"
+
+
+@pytest.fixture(name="token", autouse=True)
+def fixture_token(clear_token):
+    hashed_token = hashlib.sha256(clear_token.encode()).hexdigest()
+    name = "user"
+    permission = "user"
+    return Token.create(name=name, permission=permission, hashedToken=hashed_token)
