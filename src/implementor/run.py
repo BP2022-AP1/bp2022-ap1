4142411fd5519997afd4b53d346f1bae8c4556f4
antonykamp <info@antonykamp.de>
2023-05-04 13:02:02 +0200

    feat: implement get_all_run_ids for Run

--- src/implementor/run.py
+++ src/implementor/run.py
@@ -3,8 +3,10 @@
 
 import json
 
+from src.implementor.models import Run, SimulationConfiguration, Token
 
-def get_all_run_ids(options, token):
+
+def get_all_run_ids(options: dict, token: Token):
     """
     :param options: A dictionary containing all the paramters for the Operations
         options["simulationId"]
@@ -12,10 +14,28 @@ def get_all_run_ids(options, token):
 
     """
 
-    # Implement your business logic here
-    # All the parameters are present in the options argument
+    # Return all runs of a single simulation configuration
+    simulation_configuration_key = "simulationId"
+    if (
+        simulation_configuration_key in options
+        and options[simulation_configuration_key] is not None
+    ):
+        simulation_id = options[simulation_configuration_key]
+        simulation_configurations = SimulationConfiguration.select().where(
+            SimulationConfiguration.id == simulation_id
+        )
 
-    return json.dumps(""), 501  # 200
+        if not simulation_configurations.exists():
+            return "Simulation not found", 404
+
+        simulation_configuration = simulation_configurations.get()
+        runs = simulation_configuration.runs
+
+        runs_string = [str(run.id) for run in runs]
+        return runs_string, 200
+
+    runs = [str(run.id) for run in Run.select()]
+    return runs, 200
 
 
 def create_run(body, token):
