c41d52d08b92f0144a55731cb1e74cba20dd2d2a
antonykamp <info@antonykamp.de>
2023-05-05 14:53:46 +0200

    feat: replace Thread logic with celery in communicator

--- src/communicator/communicator.py
+++ src/communicator/communicator.py
@@ -26,26 +26,13 @@ celery.conf.accept_content = [
 ]
 
 
-class Communicator(Thread):
+class Communicator:
     """Component used for communicating with a sumo simulation using traci."""
 
     _configuration = None
     _port = None
     _components = None
-    _current_tick = 1
     _max_tick = None
-
-    _stopped = False
-    sumo_running = False
-
-    @property
-    def progress(self):
-        """Get the current simulation progress as a float between 0 and 1
-
-        :return: The current simulation progress
-        :rtype: float
-        """
-        return self._current_tick / self._max_tick
 
     def add_component(self, component: Component):
         """Add the given component to the simulation.
@@ -66,34 +53,78 @@ class Communicator(Thread):
         ),
     ):
         """Creates a new Communicator object"""
-        Thread.__init__(self)
         self._configuration = sumo_configuration
         self._port = sumo_port
         self._components = components if components is not None else []
         self._max_tick = max_tick
 
-    def run(self):
+    def run(self) -> str:
+        process = self._run.delay(
+            max_tick=self._max_tick,
+            components_pickle=pickle.dumps(self._components),
+            configuration=self._configuration,
+            port=self._port,
+        )
+        return process.id
+
+    @celery.task(bind=True, ignore_result=False)
+    def _run(
+        self: Task,
+        max_tick: int,
+        components_pickle: bytes,
+        configuration: str,
+        port: int,
+    ):
         """Starts sumo (no gui) and connects using traci. The connection has the `default` label."""
 
-        traci.start([checkBinary("sumo"), "-c", self._configuration], port=self._port)
+        components = pickle.loads(components_pickle)
+        traci.start([checkBinary("sumo"), "-c", configuration], port=port)
 
-        self.sumo_running = True
+        sumo_running = True
+        current_tick = 1
 
-        while not self._stopped and self._current_tick <= self._max_tick:
-            for component in self._components:
-                component.next_tick(self._current_tick)
-            self._simulation_step()
-            self._current_tick += 1
+        def update_state():
+            self.update_state(
+                state="PROGRESS",
+                meta={
+                    "sumo_running": sumo_running,
+                    "current": current_tick,
+                    "total": max_tick,
+                },
+            )
+
+        update_state()
+
+        while current_tick <= max_tick:
+            for component in components:
+                component.next_tick(current_tick)
+            traci.simulationStep()
+            current_tick += 1
+            update_state()
 
         traci.close(wait=False)
 
-        self.sumo_running = False
+        sumo_running = False
+        update_state()
 
-    def stop(self):
+    @classmethod
+    def stop(cls, process_id: str):
         """Stopps the simulation requesting a simulation step.
         At most, one simulation step will happen after this request"""
-        self._stopped = True
+        process = AsyncResult(process_id)
+        process.revoke()
 
-    def _simulation_step(self):
-        """Advances the simulation by one step"""
-        traci.simulationStep()
+    @classmethod
+    def progress(cls, process_id: str) -> float:
+        """Get the current simulation progress as a float between 0 and 1
+
+        :return: The current simulation progress
+        :rtype: float
+        """
+        process = AsyncResult(str(process_id))
+        return process.info.get("current") / process.info.get("total")
+
+    @classmethod
+    def state(cls, progress_id: str) -> str:
+        process = AsyncResult(progress_id)
+        return process.status
