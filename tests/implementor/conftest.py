a42e1d1061ade4c8a1c071cc67c1a6ecc96d1b60
antonykamp <info@antonykamp.de>
2023-05-08 17:51:03 +0200

    fix: tests

--- tests/implementor/conftest.py
+++ tests/implementor/conftest.py
@@ -251,17 +251,20 @@ def train(train_add) -> Train:
 
 
 @pytest.fixture
-def train_prio_fault_configuration(
-    train: Train,
-) -> TrainPrioFaultConfiguration:
-    return TrainPrioFaultConfiguration.create(
-        start_tick=50,
-        end_tick=500,
-        description="test TrainPrioFault",
-        affected_element_id=train.identifier,
-        new_prio=3,
-        strategy="regular",
-    )
+def train_prio_fault_configuration_data(train: Train) -> dict:
+    return {
+        "start_tick": 50,
+        "end_tick": 500,
+        "description": "test TrainPrioFault",
+        "affected_element_id": train.identifier,
+        "new_prio": 3,
+        "strategy": "regular",
+    }
+
+
+@pytest.fixture
+def train_prio_fault_configuration(train_prio_fault_configuration_data):
+    return TrainPrioFaultConfiguration.create(**train_prio_fault_configuration_data)
 
 
 # ------------- TrainSpeedLimitFault ----------------
@@ -295,26 +298,6 @@ def train_add(monkeypatch):
 # pylint: disable-next=unused-argument
 def train(train_add) -> Train:
     return Train(identifier="fault injector train", train_type="cargo")
-
-
-# ------------- TrainPrioFaultConfiguration ----------------
-
-
-@pytest.fixture
-def train_prio_fault_configuration_data(train: Train) -> dict:
-    return {
-        "start_tick": 50,
-        "end_tick": 500,
-        "description": "test TrainPrioFault",
-        "affected_element_id": train.identifier,
-        "new_prio": 3,
-        "strategy": "regular",
-    }
-
-
-@pytest.fixture
-def train_prio_fault_configuration(train_prio_fault_configuration_data):
-    return TrainPrioFaultConfiguration.create(train_prio_fault_configuration_data)
 
 
 # ------------- TrainSpeedFaultConfiguration ----------------
