a2b45283ab675f17d638a3058154d12630f14c6c
Anton Persitzky <82519211+instant-sky@users.noreply.github.com>
2023-05-08 13:29:13 +0200

    feat: add TrainSpeedFault related fixtures to conftest

--- tests/implementor/conftest.py
+++ tests/implementor/conftest.py
@@ -1,10 +1,14 @@
 import pytest
+from traci import vehicle
 
 from src.fault_injector.fault_configurations.track_speed_limit_fault_configuration import (
     TrackSpeedLimitFaultConfiguration,
 )
+from src.fault_injector.fault_configurations.train_speed_fault_configuration import (
+    TrainSpeedFaultConfiguration,
+)
 from src.implementor.models import SimulationConfiguration
-from src.wrapper.simulation_objects import Edge, Track
+from src.wrapper.simulation_objects import Edge, Track, Train
 
 # ------------- TrackSpeedLimitFaultConfiguration ----------------
 
@@ -45,6 +49,34 @@ def track_speed_limit_fault_configuration(
     return TrackSpeedLimitFaultConfiguration.create(
         track_speed_limit_fault_configuration_data
     )
+
+
+# ------------- TrainSpeedFaultConfiguration ----------------
+
+
+@pytest.fixture
+# pylint: disable-next=unused-argument
+def train(train_add) -> Train:
+    return Train(identifier="fault injector train", train_type="cargo")
+
+
+@pytest.fixture
+def train_speed_fault_configuration_data(train: Train) -> dict:
+    return {
+        "start_tick": 40,
+        "end_tick": 400,
+        "description": "test TrainSpeedFault",
+        "affected_element_id": train.identifier,
+        "new_speed": 30,
+        "strategy": "regular",
+    }
+
+
+@pytest.fixture
+def train_speed_fault_configuration(
+    track_speed_limit_fault_configuration_data, train: Train
+):
+    return TrainSpeedFaultConfiguration.create(train_speed_fault_configuration_data)
 
 
 # ------------- SimulationConfiguration ----------------
