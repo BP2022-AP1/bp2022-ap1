67018b55504fc6981d7a1fc630c2e4859de2b735
antonykamp <info@antonykamp.de>
2023-05-11 12:52:39 +0200

    feat: add script to add simple simulation config

---
+++ scripts/insert_simple_simulation.py
@@ -0,0 +1,47 @@
+from src.implementor.models import SimulationConfiguration
+
+from src.schedule.schedule_configuration import (
+    ScheduleConfiguration,
+    ScheduleConfigurationXSimulationPlatform,
+)
+from src.spawner.spawner import (
+    SpawnerConfiguration,
+    SpawnerConfigurationXSchedule,
+    SpawnerConfigurationXSimulationConfiguration,
+)
+from src.base_model import db
+
+with db.atomic():
+    platforms = ["bs_0", "bs_6"]
+    regular_schedule = ScheduleConfiguration.create(
+        schedule_type="TrainSchedule",
+        strategy_type="RegularScheduleStrategy",
+        strategy_start_tick=1000,
+        strategy_end_tick=2000,
+        train_schedule_train_type="passenger",
+        regular_strategy_frequency=100,
+    )
+    print(f"regular schedule: {regular_schedule.id}")
+    for index, platform in enumerate(platforms):
+        ScheduleConfigurationXSimulationPlatform.create(
+            schedule_configuration_id=regular_schedule,
+            simulation_platform_id=platform,
+            index=index,
+        )
+
+    spawner_configuration = SpawnerConfiguration()
+    spawner_configuration.save()
+    print(f"schedule configuration: {spawner_configuration.id}")
+
+    SpawnerConfigurationXSchedule(
+        spawner_configuration_id=spawner_configuration,
+        schedule_configuration_id=regular_schedule,
+    ).save()
+
+    simulation_configuration = SimulationConfiguration.create()
+    print(f"simulation configuration: {simulation_configuration.id}")
+
+    SpawnerConfigurationXSimulationConfiguration.create(
+        simulation_configuration=simulation_configuration,
+        spawner_configuration=spawner_configuration,
+    )
