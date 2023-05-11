05a471b8a071619351425e2f0b7b2dcfbaf5d0b3
CR1337 <captain_2@gmx.de>
2023-05-11 17:18:24 +0200

    refactor: changed strenum to enum

--- src/event_bus/event.py
+++ src/event_bus/event.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass
-from enum import StrEnum, auto
+from enum import Enum, auto
 
 
-class EventType(StrEnum):
+class EventType(Enum):
     TrainSpawn = auto()
     TrainRemove = auto()
     TrainArrival = auto()
