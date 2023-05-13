a76d57ec8490005a31b762e41e9792d417f5d320
Lucas Reisener <lucasreisener@gmail.com>
2023-05-13 21:23:33 +0200

    create coal demand by config id and coal train spawn by config id functions to data science

--- src/schedule/demand_schedule_strategy.py
+++ src/schedule/demand_schedule_strategy.py
@@ -53,7 +53,7 @@ class DemandScheduleStrategy(ScheduleStrategy):
     power_station: str
     scaling_factor: float
     start_datetime: datetime
-    _spawn_ticks: list[int]
+    spawn_ticks: list[int]
     _api: SmardApi
 
     def __init__(
@@ -76,9 +76,9 @@ class DemandScheduleStrategy(ScheduleStrategy):
         self.power_station = power_station
         self.scaling_factor = scaling_factor
         self.start_datetime = start_datetime
-        self._spawn_ticks = []
+        self.spawn_ticks = []
         self._api = SmardApi()
-        self._calculate_spawn_ticks()
+        self.calculate_spawn_ticks()
 
     def compute_coal_consumption(self, produced_electrical_energy: float) -> float:
         """Computes the coal consumption for the given amount of electrical energy
@@ -112,7 +112,7 @@ class DemandScheduleStrategy(ScheduleStrategy):
         coal_consumption = self.compute_coal_consumption(produced_electrical_power)
         return coal_consumption / self.COAL_PER_TRAIN
 
-    def _calculate_spawn_ticks(self):
+    def calculate_spawn_ticks(self):
         """Calculates the ticks at which trains should spawn."""
         end_datetime = self.start_datetime + timedelta(
             seconds=self.end_tick - self.start_tick
@@ -123,10 +123,10 @@ class DemandScheduleStrategy(ScheduleStrategy):
             train_accumulator += self._compute_trains_to_spawn(entry.value)
             tick = int(quarter_hour * self.SECONDS_PER_QUARTER_HOUR + self.start_tick)
             while train_accumulator >= 1.0:
-                self._spawn_ticks.append(tick)
+                self.spawn_ticks.append(tick)
                 train_accumulator -= 1.0
                 tick += 1
 
     def should_spawn(self, tick: int) -> bool:
         """Returns whether a train should spawn at the given tick."""
-        return super().should_spawn(tick) and tick in self._spawn_ticks
+        return super().should_spawn(tick) and tick in self.spawn_ticks
