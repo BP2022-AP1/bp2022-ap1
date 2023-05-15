c238c664c680f8681ce5e8680caaab6fa97229c2
MaxLietze <Max.Lietze@student.hpi.de>
2023-05-15 16:36:56 +0200

    rename edges

--- src/interlocking_component/router.py
+++ src/interlocking_component/router.py
@@ -1,26 +1,26 @@
 from typing import List
 
-from src.wrapper.simulation_objects import Node, Track
+from src.wrapper.simulation_objects import Node, Edge
 
 
 class Router:
     """This class calculates routes for trains from track segment to track segment."""
 
-    def get_route(self, start_track: Track, end_track: Track) -> List[Node]:
+    def get_route(self, start_edge: Edge, end_edge: Edge) -> List[Node]:
         """This method returns a route from a start track_segment to an end track_segment.
         It returns a list of Sumo Nodes from the Signal the train is currently driving
         toward to the Node right after the end track.
 
-        :param start_track: The track segment where the route will begin.
-        :type start_track: Track
-        :param end_track: The track segment where the route will end.
-        :type end_track: Track
-        :return: list of Nodes from the Signal after the start track
-        to the Node right after the end track
+        :param start_edge: The edge where the route will begin.
+        :type start_edge: Edge
+        :param end_edge: The edge where the route will end.
+        :type end_edge: Edge
+        :return: list of Nodes from the Signal after the start edge
+        to the Node right after the end edge
         :rtype: List[Node]
         """
-        start_node = start_track.to_node
-        penultimate_node = end_track.from_node
+        start_node = start_edge.to_node
+        penultimate_node = end_edge.from_node
 
         # The next part is dijkstra as a first mvp.
         # This will lead to deadlocks if two trains drive in opposite directions.
@@ -47,7 +47,7 @@ class Router:
                 ):
                     distances[track.to_node] = distance_to_next_node
             current_index += 1
-        route = List[penultimate_node, end_track.to_node]
+        route = List[penultimate_node, end_edge.to_node]
         while current_node in previous_nodes:
             previous_node = previous_nodes[current_node]
             route.insert(0, previous_node)
