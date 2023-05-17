e2665de4a7bd4bcf99719d9ec81a1fbb3981e7e0
Anton Persitzky <anton.persitzky@student.hpi.uni-potsdam.de>
2023-05-17 17:18:06 +0200

    feat: add tests

--- tests/api/test_decorator.py
+++ tests/api/test_decorator.py
@@ -40,7 +40,7 @@ TOKEN_HEADER = "bp2022-ap1-api-key"
         ),
         (
             "/component/fault-injection/train-speed-fault/00000000-0000-0000-0000-000000000000",
-            501,
+            404,
         ),
         (
             "/component/fault-injection/track-speed-limit-fault/00000000-0000-0000-0000-000000000000",
