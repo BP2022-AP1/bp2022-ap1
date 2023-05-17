00732ad8a1576db098e9492dacab7b86b27fa6e2
Anton <82519211+instant-sky@users.noreply.github.com>
2023-05-17 20:11:18 +0200

    Add functionality to /component/fault-injection/track-speed-limit-fault/{id} path (#476)

--- src/implementor/component.py
+++ src/implementor/component.py
@@ -261,29 +261,45 @@ def create_track_speed_limit_fault_configuration(body, token):
 def get_track_speed_limit_fault_configuration(options, token):
     """
     :param options: A dictionary containing all the parameters for the Operations
-        options["id"]
+        options["identifier"]
     :param token: Token object of the current user
 
     """
 
-    # Implement your business logic here
-    # All the parameters are present in the options argument
-
-    return json.dumps("<map>"), 501  # 200
+    identifier = options["identifier"]
+    configs = TrackSpeedLimitFaultConfiguration.select().where(
+        TrackSpeedLimitFaultConfiguration.id == identifier
+    )
+    if not configs.exists():
+        return "Id not found", 404
+    config = configs.get()
+    return config.to_dict(), 200
 
 
 def delete_track_speed_limit_fault_configuration(options, token):
     """
     :param options: A dictionary containing all the parameters for the Operations
-        options["id"]
+        options["identifier"]
     :param token: Token object of the current user
 
     """
 
-    # Implement your business logic here
-    # All the parameters are present in the options argument
+    identifier = options["identifier"]
+    configs = TrackSpeedLimitFaultConfiguration.select().where(
+        TrackSpeedLimitFaultConfiguration.id == identifier
+    )
+    if not configs.exists():
+        return "Id not found", 404
+    config = configs.get()
 
-    return "", 501  # 204
+    if config.simulation_configuration_references.exists():
+        return (
+            "Track speed limit fault configuration is referenced by a simulation configuration",
+            400,
+        )
+
+    config.delete_instance()
+    return "Deleted track-speed-limit-fault configuration", 204
 
 
 def get_all_train_prio_fault_configuration_ids(options, token):
--- tests/api/test_decorator.py
+++ tests/api/test_decorator.py
@@ -44,7 +44,7 @@ TOKEN_HEADER = "bp2022-ap1-api-key"
         ),
         (
             "/component/fault-injection/track-speed-limit-fault/00000000-0000-0000-0000-000000000000",
-            501,
+            404,
         ),
     ],
 )
