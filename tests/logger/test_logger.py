b5f0a520f67768cd72a59ed6996f6c037a03893b
CR1337 <captain_2@gmx.de>
2023-05-23 11:26:51 +0200

    fix: message in test

--- tests/logger/test_logger.py
+++ tests/logger/test_logger.py
@@ -256,8 +256,7 @@ class TestLogger:
         assert log_entry.tick == tick
         assert (
             log_entry.message
-            == f"Train with ID {train_id} left block section with ID {block_section_id} with "
-            f"length {block_section_length}"
+            == f"Train with ID {train_id} left block section with ID {block_section_id}"
         )
         assert log_entry.run_id.id == run.id
         assert log_entry.train_id == train_id
