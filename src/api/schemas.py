c6322bf1520094c57dea5f08b49ba6044efda9d5
Antony Kamp <antony.kamp@student.hpi.de>
2023-05-30 13:26:12 +0200

    Feature: Add /schedule/cole-demand and /schedule/cole-demand/:id endpoints (#503)
    * feat: add endpoint for cole demand schedules
    
    * feat: test endpoint cole demand schedules

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -20,6 +20,18 @@ class ScheduleConfiguration(Schema):
 
     schedule_type = fields.String(required=True)
     platforms = fields.List(fields.String(), required=True)
+    strategy_start_tick = fields.Integer()
+    strategy_end_tick = fields.Integer()
+
+    train_schedule_train_type = fields.String()
+
+
+class CoalDemandScheduleConfiguration(ScheduleConfiguration):
+    """The marshmallow schema for the coal demand schedule configuration"""
+
+    demand_strategy_power_station = fields.String(required=True)
+    demand_strategy_scaling_factor = fields.Float(required=True)
+    demand_strategy_start_datetime = fields.DateTime(required=True)
 
 
 class SimulationConfiguration(Schema):
