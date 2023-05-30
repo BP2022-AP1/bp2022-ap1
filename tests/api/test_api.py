2b18eaec49a05c201e3f6788a5383edd597e14b5
antonykamp <info@antonykamp.de>
2023-05-30 13:06:26 +0200

    test: add for existence of first token

---
+++ tests/api/test_api.py
@@ -0,0 +1,9 @@
+import os
+from src.implementor.token import hash_token
+from src.implementor.models import Token
+
+
+def test_first_token(client):
+    clear_token = os.getenv("FIRST_ADMIN_TOKEN")
+    hashed_token = hash_token(clear_token)
+    assert Token.select().where(Token.hashedToken == hashed_token).exists()
