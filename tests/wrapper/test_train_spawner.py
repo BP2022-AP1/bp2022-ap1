41ee851ef9619b2d6990490e008e8b5711d5ba29
Lietze <50149435+Lietze@users.noreply.github.com>
2023-06-02 14:08:41 +0200

    Feature/438 implement deadlock avoiding by dynamic route reservation algorithm (#466)
    * first steps
    
    * more changes
    
    * more stuff
    
    * more stuff
    
    * reservation are no also removed
    
    * lint
    
    * fix test
    
    * More stuff
    
    * a lot of stuff
    
    * format and lint
    
    * fix test and lint
    
    * fix annother test
    
    * fix format
    
    * format
    
    * reset routes to be reserved when fault is injected
    
    * more assertions to catch skipped tracks
    
    * readd token to run
    
    * format
    
    * lint
    
    * fix test
    
    * format again

--- tests/wrapper/test_train_spawner.py
+++ tests/wrapper/test_train_spawner.py
@@ -7,18 +7,26 @@ from src.wrapper.train_builder import TrainBuilder
 
 
 def test_spawn_train(spawner: Tuple[SimulationObjectUpdatingComponent, TrainBuilder]):
-    (souc, spawner) = spawner
-    assert spawner.spawn_train("test-train-1", ["station-1", "station-2"], "test-cargo")
+    (souc, train_spawner) = spawner
+    assert train_spawner.spawn_train(
+        "test-train-1", ["station-1", "station-2"], "test-cargo"
+    )
 
     assert len(souc.trains) == 1
+
+    assert train_spawner.route_controller.set_spawn_fahrstrasse_count == 1
+    assert train_spawner.route_controller.reserve_for_initialized_train_count == 1
 
 
 def test_spawn_train_no_route(
     spawner: Tuple[SimulationObjectUpdatingComponent, TrainBuilder]
 ):
-    (souc, spawner) = spawner
-    assert not spawner.spawn_train(
+    (souc, train_spawner) = spawner
+    assert not train_spawner.spawn_train(
         "test-train-2", ["station-2", "station-0"], "test-cargo"
     )
 
     assert len(souc.trains) == 0
+
+    assert train_spawner.route_controller.set_spawn_fahrstrasse_count == 1
+    assert train_spawner.route_controller.reserve_for_initialized_train_count == 0
