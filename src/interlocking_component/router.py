e311fad18723a915abd3b8b224a564b3b1009f12
Lietze <50149435+Lietze@users.noreply.github.com>
2023-06-02 11:25:08 +0200

    Routing considers if a track is blocked (#535)
    * The feature
    
    * fix lint

--- src/interlocking_component/router.py
+++ src/interlocking_component/router.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Dict, List
 
 from src.wrapper.simulation_objects import Edge, Node
 
@@ -19,27 +19,29 @@ class Router:
         to the Node right after the end edge
         :rtype: List[Node]
         """
-        start_node = start_edge.to_node
-        penultimate_node = end_edge.from_node
-
         # The next part is dijkstra as a first mvp.
         # This will lead to deadlocks if two trains drive in opposite directions.
-        distances = {}
-        previous_nodes = {}
-        distances[start_node] = 0
-        previous_nodes[start_node] = start_edge.from_node
+        distances: Dict[Node, int] = {}
+        previous_nodes: Dict[Node, Node] = {}
+        distances[start_edge.to_node] = 0
+        previous_nodes[start_edge.to_node] = start_edge.from_node
         current_index = 0
         while True:
             sorted_distances = sorted(distances.items(), key=lambda item: item[1])
             # This sorts the distances based on the values in the dict.
             # sorted_distances is a array of the dict with items of the dict as tupels.
-            current_node = sorted_distances[current_index][0]
-            if current_node == penultimate_node:
+            try:
+                current_node = sorted_distances[current_index][0]
+            except Exception as exc:
+                raise ValueError("No route could be found.") from exc
+            if current_node == end_edge.from_node:
                 break
             edge_to_current_node = previous_nodes[current_node].get_edge_to(
                 current_node
             )
             for edge in current_node.get_edges_accessible_from(edge_to_current_node):
+                if edge.blocked is True:
+                    continue
                 distance_to_next_node = distances[current_node] + edge.length
                 if (
                     edge.to_node not in distances
@@ -48,12 +50,12 @@ class Router:
                     distances[edge.to_node] = distance_to_next_node
                     previous_nodes[edge.to_node] = current_node
             current_index += 1
-        route = [penultimate_node, end_edge.to_node]
+        route = [end_edge.from_node, end_edge.to_node]
         while current_node in previous_nodes:
             previous_node = previous_nodes[current_node]
             route.insert(0, previous_node)
             current_node = previous_node
-            if current_node == start_node:
+            if current_node == start_edge.to_node:
                 route.insert(0, previous_nodes[current_node])
                 break
         return route
