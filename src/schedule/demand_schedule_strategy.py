3c37bd228e2c9ca5fc0533756938ef656ad11489
Christian Raue <captain_2@gmx.de>
2023-07-04 13:09:55 +0200

    fix: line too long

--- src/schedule/demand_schedule_strategy.py
+++ src/schedule/demand_schedule_strategy.py
@@ -30,7 +30,8 @@ class DemandScheduleStrategy(ScheduleStrategy):
     # [see result from research/coal_consumption/coal_consumption.ipynb section 46]
 
     COAL_ENERGY_CONTENT: float = 2.46  # in MWh/t
-    # [source: hhttps://braunkohle.de/wp-content/uploads/2019/08/Bodenschatz-Braunkohle-%E2%80%93-Langfassung.pdf]
+    # [source: hhttps://braunkohle.de/wp-content/uploads/2019/08
+    # /Bodenschatz-Braunkohle-%E2%80%93-Langfassung.pdf]
 
     SECONDS_PER_QUARTER_HOUR: int = (60 / 4) * 60
     COAL_PER_WAGGON: float = 60  # in tons [source: interview]
