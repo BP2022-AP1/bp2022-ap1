3ef53f8abd0c5bee6338601a949666af184ef2dd
Christian Raue <captain_2@gmx.de>
2023-07-04 13:07:17 +0200

    changed coal energy content

--- src/schedule/demand_schedule_strategy.py
+++ src/schedule/demand_schedule_strategy.py
@@ -29,8 +29,8 @@ class DemandScheduleStrategy(ScheduleStrategy):
     TOTAL_ELECTRICAL_POWER: float = 10632.75  # in MW
     # [see result from research/coal_consumption/coal_consumption.ipynb section 46]
 
-    COAL_ENERGY_CONTENT: float = 5.56  # in MWh/t (= 20 MJ/kg)
-    # [source: https://www.energie-lexikon.info/kohle.html]
+    COAL_ENERGY_CONTENT: float = 2.46  # in MWh/t
+    # [source: hhttps://braunkohle.de/wp-content/uploads/2019/08/Bodenschatz-Braunkohle-%E2%80%93-Langfassung.pdf]
 
     SECONDS_PER_QUARTER_HOUR: int = (60 / 4) * 60
     COAL_PER_WAGGON: float = 60  # in tons [source: interview]
