0eefea431fde95dfd94d4c404e81279f79012319
MaxLietze <Max.Lietze@student.hpi.de>
2023-07-24 17:42:10 +0200

    clean up

--- src/api/run.py
+++ src/api/run.py
@@ -9,37 +9,41 @@ bp = Blueprint("run", __name__)
 
 
 @bp.route("/run", methods=["get"])
-def get_all_run_ids():
+@token_required()
+def get_all_run_ids(token):
     """Get all run id"""
     options = {}
     options["simulationId"] = request.args.get("simulationId")
 
-    return impl.run.get_all_run_ids(options, None)
+    return impl.run.get_all_run_ids(options, token)
 
 
 @bp.route("/run", methods=["post"])
-def create_run():
+@token_required()
+def create_run(token):
     """Create a run"""
     schema = schemas.RunConfiguration()
 
     body = parser.parse(schema, request, location="json")
 
-    return impl.run.create_run(body, None)
+    return impl.run.create_run(body, token)
 
 
 @bp.route("/run/<identifier>", methods=["get"])
-def get_run(identifier):
+@token_required()
+def get_run(identifier, token):
     """Get a run"""
     options = {}
     options["identifier"] = identifier
 
-    return impl.run.get_run(options, None)
+    return impl.run.get_run(options, token)
 
 
 @bp.route("/run/<identifier>", methods=["delete"])
-def delete_run(identifier):
+@token_required()
+def delete_run(identifier, token):
     """Delete a run"""
     options = {}
     options["identifier"] = identifier
 
-    return impl.run.delete_run(options, None)
+    return impl.run.delete_run(options, token)
